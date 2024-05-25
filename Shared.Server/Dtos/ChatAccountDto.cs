4b97b9b82e43c2bdad2e35957a683d6c4914276f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-05-25 10:11:56 +0330

    create : basic chat page

---
+++ Shared.Server/Dtos/ChatAccountDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Server.Dtos;
+public class ChatAccountDto {
+    public string LogoUrl { get; set; } = String.Empty;
+    public string FullName { get; set; } = String.Empty;
+    public int UnReadMessages { get; set; } = 0;
+
+
+}
