5180d8fae1dcab4be66f72c1866f2856c500ecd8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 15:23:49 +0330

    remove : because of refactoring

--- Service/ChatService/Applications/Apps.Chats/Commands/IChatItemCommands.cs
+++
@@ -1,9 +0,0 @@
-﻿using Shared.Server.Models.Results;
-
-namespace Apps.Chats.Commands;
-
-public interface IChatItemCommands
-{
-    Task<Result> CreateAsync(Guid requesterId, Guid receiverId);
-    Task<Result> DeleteAsync(Guid chatId);
-}
--- Service/ChatService/Applications/Apps.Chats/Commands/IChatMessageCommands.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domains.Chat.MessageAggregate;
-using Shared.Server.Models.Results;
-
-namespace Apps.Chats.Commands.Impls;
-
-public interface IChatMessageCommands {
-    Task<Result> SendAsync(ChatMessage chatMessage);
-    Task<Result> DeleteAsync(Guid messageId);
-}
