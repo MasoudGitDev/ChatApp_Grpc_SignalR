7b97700116124dee2ee06a99632c7ae51bf9042d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 09:40:13 +0330

    create : IChatUOW

---
+++ Service/ChatService/Applications/Apps.Chats/IChatUOW.cs
@@ -0,0 +1,12 @@
+﻿using Apps.Chats.Abstractions;
+using Domain.Auth.UserAggregate;
+
+namespace Apps.Chats;
+public interface IChatUOW {
+    public IChatItemQueries ChatItemQueries { get; }
+    public IMessageQueries MessageQueries { get;}
+    public IUserQueries UserQueries { get; }
+    public Task CreateAsync<TEntity>(TEntity entity) where TEntity : class, new();
+    public Task DeleteAsync<TEntity>(TEntity entity) where TEntity : class, new();
+    public Task SaveChangeAsync();
+}
