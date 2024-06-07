6152e1b74ec2f887fd6897c80f6e664ced3de1bc
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 09:50:01 +0330

    update : change method returns(use AccountResult) and update Evaluate body

--- Services/Auth/Applications/Apps.Auth/Jwt/JwtService.cs
+++ Services/Auth/Applications/Apps.Auth/Jwt/JwtService.cs
@@ -6,12 +6,14 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Shared.Server.Models;
+using Shared.Server.Models.Results;
+using System.Data;
 
 namespace Apps.Auth.Jwt;
 
 public interface IJwtService {
-    Task<string> GenerateAsync(Guid userId);
-    Task<string> EvaluateAsync(string token , string userId);
+    Task<AccountResult> GenerateAsync(Guid userId);
+    Task<AccountResult> EvaluateAsync(string token , string userId);
 }
 
 internal class JwtService(JwtSettingsModel model) : IJwtService {
@@ -19,24 +21,24 @@ internal class JwtService(JwtSettingsModel model) : IJwtService {
     private readonly SymmetricSecurityKey _symmetricSecurityKey =new(Encoding.UTF8.GetBytes(model.SecureKey));
     private readonly string _alg = SecurityAlgorithms.HmacSha256Signature;
 
-    public async Task<string> EvaluateAsync(string token , string userId) {
+    public async Task<AccountResult> EvaluateAsync(string token , string userId) {
         var jwtSecurityToken = new JwtSecurityTokenHandler().ReadJwtToken(token);
         var userIdValue = jwtSecurityToken.Claims.Where(x => x.Type == TokenKeys.UserId).FirstOrDefault()?.Value
             .ThrowIfNullOrWhiteSpace("<user-id> claim value can not be NullOrWhiteSpace.")  ;
         if(userId != userIdValue) {
-            JwtException.Create("Your jwt token is invalid.");
+           throw JwtException.Create("Your jwt token is invalid.");
         }
         return await GenerateAsync(Guid.Parse(userId));
     }
 
-    public Task<string> GenerateAsync(Guid userId) {
+    public Task<AccountResult> GenerateAsync(Guid userId) {
         var tokenHandler = new JwtSecurityTokenHandler();
         var securityToken = (tokenHandler.CreateToken(DescribeToken(userId)))
             .ThrowIfNull("<securityToken> can not be null.");
         if(!tokenHandler.CanWriteToken) {
             JwtException.Create("System can not write token.");
         }
-        return Task.FromResult(tokenHandler.WriteToken(securityToken));
+        return Task.FromResult(AccountResult.Create(tokenHandler.WriteToken(securityToken)));
     }
 
 
