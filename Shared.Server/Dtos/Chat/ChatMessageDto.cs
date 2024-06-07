7940fc1995f7e97877210b2735631779f92dcf49
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 09:18:20 +0330

    move : change ChatMessageDto file possition

---
+++ Shared.Server/Dtos/Chat/ChatMessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Server.Dtos.Chat;
+public class MessageDto
+{
+    public Guid MessageId { get; set; }
+    public Guid ChatId { get; set; }
+    public Guid SenderId { get; set; }
+    public bool IsMyMessage { get; set; } = false;
+    public string Content { get; set; } = string.Empty;
+    public string FileUrl { get; set; } = string.Empty;
+}
--- Shared.Server/Dtos/ChatMessageDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shared.Server.Dtos;
-public class ChatMessageDto {
-    public Guid UserId { get; set; }
-    public bool IsMyMessage { get; set; } = false;
-    public string Message { get; set; } = String.Empty;
-    public string LogoUrl { get; set; } = String.Empty;
-}
