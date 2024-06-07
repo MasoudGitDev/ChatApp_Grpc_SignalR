fa8fc70e3201a8924b11e27f7fe05a23ac3cc9b6
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 10:01:46 +0330

    create : register chat commands

---
+++ Service/ChatService/Applications/Apps.Chats/ServiceReg.cs
@@ -0,0 +1,19 @@
+﻿using Apps.Chats.Commands;
+using Apps.Chats.Commands.Impls;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Apps.Chats;
+public static class ServiceReg {
+
+    public static IServiceCollection AddChatServices(this IServiceCollection services) {
+        services.AddScoped<IChatItemCommands , ChatItemCommands>();
+        services.AddScoped<IChatMessageCommands , ChatMessageCommands>();
+        return services;
+    }
+
+    public static WebApplication UseChatService(this WebApplication app) {
+        return app;
+    }
+
+}
