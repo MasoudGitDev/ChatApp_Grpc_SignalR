f60cd133237b4786abae4cb9c72dbba7718840bd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 09:27:52 +0330

    create : an attribute to handle db saveChanges exceptions

---
+++ Infrastructures/Infra.EFCore/Exceptions/DbChangeExceptionAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Infra.EFCore.Exceptions;
+
+[AttributeUsage(AttributeTargets.All)]
+internal class DbChangeExceptionAttribute : Attribute, IExceptionFilter {
+    public void OnException(ExceptionContext context) {
+        if(context.Exception == null) {
+            return;
+        }
+        context.ExceptionHandled = true;
+        context.Result = new JsonResult(new { 
+            code = "OnDBSaveChanges" , 
+            where = context.ActionDescriptor.DisplayName,
+            message = context.Exception.Message 
+        });
+    }
+}
