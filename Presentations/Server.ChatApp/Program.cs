df0aac214870cdc299154267d08726fdf15f5a87
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 10:09:07 +0330

    update : add Grpc service and dependencies

--- Presentations/Server.ChatApp/Program.cs
+++ Presentations/Server.ChatApp/Program.cs
@@ -1,7 +1,10 @@
+using Apps.Chats;
+using Apps.Chats.Hubs;
 using Infra.EFCore;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.OpenApi.Models;
-using Shared.Server.Extensions;
-using Shared.Server.Models;
+using Server.ChatApp.GRPCServices;
+using Server.ChatApp.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddScoped((_) => new DBConnectionModel(
 //    builder.Configuration["GRPCChatDB"].ThrowIfNullOrWhiteSpace("The <connection-string> can not be NullOrWhiteSpace.")));
 
+builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcReflection();
+builder.Services.AddGrpcSwagger();
 builder.Services.AddSwaggerGen(opt => {
     opt.SwaggerDoc("v1" , new OpenApiInfo() { Title = "Jwt Authentication" });
     var openApiSecurityScheme = new OpenApiSecurityScheme(){
@@ -25,33 +31,67 @@ builder.Services.AddSwaggerGen(opt => {
         }
     };
     opt.AddSecurityRequirement(new OpenApiSecurityRequirement() {
-
         {openApiSecurityScheme , Array.Empty<string>() }
     });
     opt.AddSecurityDefinition("Bearer" , openApiSecurityScheme);
 
+    //var filePath = Path.Combine(System.AppContext.BaseDirectory, "Server.xml");
+    //opt.IncludeXmlComments(filePath);
+    //opt.IncludeGrpcXmlComments(filePath , includeControllerXmlComments: true);
+});
+
+//builder.Services.AddSwaggerGen(c => {
+//    c.SwaggerDoc("v1" ,
+//        new OpenApiInfo { Title = "gRPC transcoding" , Version = "v1" });
+//});
+
+builder.Services.AddResponseCompression(opts => {
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+          new[] { "application/octet-stream" });
 });
 
 builder.Services.AddEFCoreService();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddChatServices();
+builder.Services.AddSignalR();
 
 
-
+//builder.Services.AddGrpcClient<AdService.AdServiceClient>(o => o.Address = new Uri("https://localhost:7223"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+if(app.Environment.IsDevelopment()) { 
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json" , "My API V1");
+    });
 }
 
-app.UseHttpsRedirection();
+app.UseResponseCompression();
+app.UseCors(opt => {
+    opt.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+        .WithExposedHeaders("Content-Disposition" , "grpc-status" , "grpc-message");
+});
 
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseGrpcWeb();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
+
+app.MapGrpcService<GrpcChatMessageHandler>().EnableGrpcWeb();
+app.MapGrpcService<GrpcAccountHandler>().EnableGrpcWeb();
+app.MapHub<ChatMessageHub>("/chatMessageHub");
+
 app.MapControllers();
+
+app.UseChatService();
+
+
 
 app.Run();
