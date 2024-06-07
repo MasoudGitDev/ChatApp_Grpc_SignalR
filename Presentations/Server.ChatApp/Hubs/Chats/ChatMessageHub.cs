2ade6eb8ec7865b4c9bef6f07dbfe7e3df7b68c6
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 10:03:58 +0330

    create : ChatMessageHub

---
+++ Presentations/Server.ChatApp/Hubs/Chats/ChatMessageHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Shared.Server.Dtos.Chat;
+
+namespace Apps.Chats.Hubs;
+
+
+public class ChatMessageHub : Hub {
+    public async Task SendMessage(MessageDto msg) {
+        await Clients.All.SendAsync("ReceiveMessage" , msg , new CancellationToken());
+    }
+}
