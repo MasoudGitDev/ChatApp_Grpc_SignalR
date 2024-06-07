6c67a5b268359b00c90538916387d3edeeaae31e
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-07 09:23:19 +0330

    create : ChatMessage aggregate , fileUrl value object for that

---
+++ Services/ChatService/Domains/Domains.Chat/MessageAggregate/ChatMessage.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Auth.UserAggregate;
+using Domains.Chat.ChatAggregate;
+using Domains.Chat.MessageAggregate.ValueObjects;
+
+namespace Domains.Chat.MessageAggregate;
+public partial class ChatMessage{
+    public Guid Id { get; private set; }
+    public Guid ChatId { get; set; }
+    public Guid SenderId { get; private set; }
+
+    public FileUrl FileUrl { get; private set; }
+    public string Content { get; private set; } = null!;
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime? ModifiedAt { get; private set; } = null;
+
+    //=========================relations
+    public ChatItem Chat { get; private set; } = null!;
+    public AppUser Sender { get; private set; } = null!;
+}
+
+//Methods
+public partial class ChatMessage {
+   
+    public static ChatMessage Create(
+        Guid chatId ,
+        Guid senderId ,
+        string content ,
+        FileUrl? fileUrl = null ,
+        Guid? messageId = null) => new() {
+            ChatId = chatId ,
+            SenderId = senderId ,
+            Id = messageId ?? Guid.NewGuid() ,
+            Content = content ,
+            FileUrl = fileUrl ?? FileUrl.Empty
+        };
+
+    public void Update(FileUrl fileUrl) {
+        FileUrl = fileUrl;
+        ModifiedAt = DateTime.UtcNow;
+    }
+    public void Update(string content) {
+        Content = content;
+        ModifiedAt = DateTime.UtcNow;
+    }
+}
---
+++ Services/ChatService/Domains/Domains.Chat/MessageAggregate/ValueObjects/FileUrl.cs
@@ -0,0 +1,17 @@
+﻿namespace Domains.Chat.MessageAggregate.ValueObjects;
+public class FileUrl {
+    public string Value { get; set; } = string.Empty;
+
+    public FileUrl(string url) {
+        if(String.IsNullOrWhiteSpace(url))
+            throw new ArgumentNullException(nameof(url));
+        Value = url;
+    }
+
+    public static FileUrl Create(string url) => new(url);
+    public static FileUrl Empty => new("<empty>");
+
+    public static implicit operator string(FileUrl value) => value.Value;
+    public static implicit operator FileUrl(string value) => new(value);
+
+}
