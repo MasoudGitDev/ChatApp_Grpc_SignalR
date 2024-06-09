9fa270f0453af31e1556f8ea0d7fe71bd6f2bdae
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-09 10:28:34 +0330

    update : add ChatRequest and Contact configs

--- Infrastructures/Infra.EFCore/Configs/Chat/ChatConfigs.cs
+++ Infrastructures/Infra.EFCore/Configs/Chat/ChatConfigs.cs
@@ -1,13 +1,18 @@
-﻿using Domains.Chats.Item.Aggregate;
+﻿using Domains.Auth.User.Aggregate;
+using Domains.Chats.Contacts.Aggregate;
+using Domains.Chats.Item.Aggregate;
 using Domains.Chats.Message.Aggregate;
 using Domains.Chats.Message.ValueObjects;
+using Domains.Chats.Requests.Aggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infra.EFCore.Configs.Chat;
 internal class ChatConfigs :
     IEntityTypeConfiguration<ChatItem>,
-    IEntityTypeConfiguration<ChatMessage> {
+    IEntityTypeConfiguration<ChatMessage> ,
+    IEntityTypeConfiguration<Contact> ,
+    IEntityTypeConfiguration<ChatRequest>{
     public void Configure(EntityTypeBuilder<ChatItem> builder) {
         builder.HasKey(x => x.Id);
         builder.HasIndex(x => new { x.RequesterId , x.ReceiverId }).IsUnique();
@@ -33,5 +38,19 @@ internal class ChatConfigs :
         builder.HasOne(x => x.Sender).WithMany().IsRequired()
            .HasForeignKey(x => x.SenderId).IsRequired()
            .OnDelete(DeleteBehavior.NoAction);
+    }
+
+    public void Configure(EntityTypeBuilder<ChatRequest> builder) {
+        builder.ToTable("ChatRequests");
+        builder.HasKey(x => new { x.RequesterId , x.ReceiverId });
+        builder.HasOne<AppUser>().WithMany().HasForeignKey(x=> new { x.RequesterId })
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+    public void Configure(EntityTypeBuilder<Contact> builder) {
+        builder.ToTable("Contacts");
+        builder.HasKey(x => new { x.RequesterId , x.ReceiverId });
+        builder.HasOne<AppUser>().WithMany().HasForeignKey(x => new { x.RequesterId })
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
