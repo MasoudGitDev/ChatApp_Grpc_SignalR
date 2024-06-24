cbd3bb5b3d9aa90c237260fff742ba01181d2102
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 19:26:01 +0330

    update : remove other methods and just add FindById

--- Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
+++ Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
@@ -3,11 +3,6 @@ using Domains.Chats.Requests.Aggregate;
 
 namespace Apps.Chats.Queries {
    public interface IChatRequestQueries {
-
-        /// <summary>
-        /// The userId can be requester or receiver Id!
-        /// </summary>
-        public Task<AppUser?> FindAsync(Guid userId);
-        public Task<ChatRequest?> BlockAsync(Guid receiverId);
+        Task<ChatRequest?> FindByIdAsync(Guid chatRequestId);
     }
 }
