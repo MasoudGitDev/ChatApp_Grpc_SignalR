6afa2357f8f7ce0d79d57ced23b9223ccfe938f5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 21:39:34 +0330

    update : Add FindByProfileIdAsync with impl

--- Infrastructures/Infra.EFCore/Implementations/Accounts/UserQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Accounts/UserQueries.cs
@@ -1,12 +1,18 @@
 ﻿using Apps.Auth.Queries;
 using Domains.Auth.User.Aggregate;
+using Domains.Auth.User.ValueObjects;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.EFCore.Implementations.Accounts;
 
 internal class UserQueries(UserManager<AppUser> _userManager) : IUserQueries {
     public async Task<AppUser?> FindByIdAsync(Guid userId) {
         return await _userManager.FindByIdAsync(userId.ToString());
+    }
+
+    public async Task<AppUser?> FindByProfileIdAsync(ProfileId profileId) {
+        return await _userManager.Users.Where(x=> x.ProfileId == profileId).FirstOrDefaultAsync();
     }
 
     public async Task<AppUser?> FindByUserNameAsync(string username) {
