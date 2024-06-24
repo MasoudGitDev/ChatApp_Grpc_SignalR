f5b15bb6ba46cfd9fbfafde4f40639316d27bda2
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 18:54:42 +0330

    create : IAppQueries interface and it impl

---
+++ Infrastructures/Infra.EFCore/Implementations/AppQueries.cs
@@ -0,0 +1,17 @@
+﻿using Apps.Auth.Queries;
+using Apps.Chats.Queries;
+
+namespace Infra.EFCore.Implementations;
+internal sealed class AppQueries(
+    IUserQueries _users ,
+    IChatItemQueries _chatItems ,
+    IChatMessageQueries _messages ,
+    IChatRequestQueries _requests ,
+    IContactQueries _contacts
+    ) : IAppQueries {
+    public IUserQueries Users => _users;
+    public IChatRequestQueries ChatRequests => _requests;
+    public IChatItemQueries ChatItems => _chatItems;
+    public IChatMessageQueries ChatMessages => _messages;
+    public IContactQueries Contacts => _contacts;
+}
---
+++ Services/Chats/Apps.Chats/Queries/IAppQueries.cs
@@ -0,0 +1,11 @@
+﻿using Apps.Auth.Queries;
+
+namespace Apps.Chats.Queries;
+
+public interface IAppQueries {
+    IUserQueries Users { get; }
+    IChatRequestQueries ChatRequests { get; }
+    IChatItemQueries ChatItems { get; }
+    IChatMessageQueries ChatMessages { get; }
+    IContactQueries Contacts { get; }
+}
