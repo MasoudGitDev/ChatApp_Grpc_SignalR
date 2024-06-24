c152f00d40b0e7852e41e7f133655485e3c2543a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 21:33:48 +0330

    create : ProfileId ValueObject to search profile by other users

---
+++ Services/Auth/Domains.Auth/User/ValueObjects/ProfileId.cs
@@ -0,0 +1,14 @@
+﻿namespace Domains.Auth.User.ValueObjects;
+public class ProfileId {
+    public string Value { get; private set; } = String.Empty;
+
+    public ProfileId(string profileId) {
+        if(String.IsNullOrWhiteSpace(profileId)) {
+            throw new ArgumentNullException(nameof(profileId));
+        }
+        Value = profileId;
+    }
+
+    public static implicit operator ProfileId(string profileId) => new(profileId);
+    public static implicit operator string(ProfileId profileId) => profileId.Value;
+}
