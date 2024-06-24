4ff15637a8e9ac5a6d4280fcd57760ba7269d919
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 19:27:38 +0330

    update : add new db sets (Contacts and ChatRequests)

--- Infrastructures/Infra.EFCore/Contexts/AppDbContext.cs
+++ Infrastructures/Infra.EFCore/Contexts/AppDbContext.cs
@@ -1,7 +1,9 @@
 ﻿using Domains.Auth.Role.Aggregate;
 using Domains.Auth.User.Aggregate;
+using Domains.Chats.Contacts.Aggregate;
 using Domains.Chats.Item.Aggregate;
 using Domains.Chats.Message.Aggregate;
+using Domains.Chats.Requests.Aggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +25,8 @@ internal class AppDbContext : IdentityDbContext<AppUser , AppRole , Guid> {
     public DbSet<AppUser> AppUsers { get; set; }
     public DbSet<ChatItem> ChatItems { get; set; }
     public DbSet<ChatMessage> ChatMessages { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<ChatRequest> ChatRequests { get; set; }
 
 
 }
