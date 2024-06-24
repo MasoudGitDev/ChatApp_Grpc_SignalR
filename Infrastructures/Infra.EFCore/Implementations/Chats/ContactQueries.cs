dafc72d79fee8743ca4c7fd4b8937c6fd12f6978
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 19:06:41 +0330

    create : IContactQueries and its class (without impls)

---
+++ Infrastructures/Infra.EFCore/Implementations/Chats/ContactQueries.cs
@@ -0,0 +1,18 @@
+﻿using Apps.Chats.Queries;
+using Domains.Chats.Contacts.Aggregate;
+using Infra.EFCore.Contexts;
+
+namespace Infra.EFCore.Implementations.Chats;
+internal sealed class ContactQueries(AppDbContext _dbContext) : IContactQueries {
+    public Task<Contact?> FindAsync(Guid contactId) {
+        throw new NotImplementedException();
+    }
+
+    public Task<Contact?> FindByUserIdAsync(Guid userId) {
+        throw new NotImplementedException();
+    }
+
+    public Task<Contact?> IsInContactAsync(Guid userId1 , Guid userId2) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/Chats/Apps.Chats/Queries/IContactQueries.cs
@@ -0,0 +1,17 @@
+﻿using Domains.Chats.Contacts.Aggregate;
+
+namespace Apps.Chats.Queries;
+public interface IContactQueries {
+  
+    Task<Contact?> FindAsync(Guid contactId);
+
+    /// <summary>
+    /// The userId can be receiverId or requesterId.
+    /// </summary>
+    Task<Contact?> FindByUserIdAsync(Guid userId);
+
+    /// <summary>
+    /// Each one of userId1 or userId2 can be receiverId or requesterId.
+    /// </summary>
+    Task<Contact?> IsInContactAsync(Guid userId1 , Guid userId2);
+}
