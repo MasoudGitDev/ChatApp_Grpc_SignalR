82127098137b172dde1d79a3774bbdef1226512f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-24 23:46:39 +0330

    refactor : change messages (models) in in and out of methods

--- Presentations/Server.ChatApp/GRPCHandlers/ChatRequestHandler.cs
+++ Presentations/Server.ChatApp/GRPCHandlers/ChatRequestHandler.cs
@@ -12,13 +12,13 @@ namespace Server.ChatApp.GRPCHandlers;
 public class ChatRequestHandler(IChatUOW _unitOfWork) : ChatRequestRPCs.ChatRequestRPCsBase
 {
 
-    public override async Task<ChatRequestRes> Request(SendRequest request, ServerCallContext context)
+    public override async Task<ResultMsg> Request(UsersConnection request, ServerCallContext context)
     {
         await _unitOfWork.CreateAsync(ChatRequest.Create(request.RequesterId.AsGuid(), request.ReceiverId.AsGuid()));
         return DefaultResult;
     }
 
-    public override async Task<ChatRequestRes> Accept(SharedRequest request, ServerCallContext context)
+    public override async Task<ResultMsg> Accept(ChatRequestMsg request , ServerCallContext context)
     {
         return await ApplyAsync(request, async (model) =>
         {
@@ -27,17 +27,17 @@ public class ChatRequestHandler(IChatUOW _unitOfWork) : ChatRequestRPCs.ChatRequ
         });
     }
 
-    public override async Task<ChatRequestRes> Block(SharedRequest request, ServerCallContext context)
+    public override async Task<ResultMsg> Block(ChatRequestMsg request , ServerCallContext context)
     {
         return await ApplyAsync(request, async (model) => await model.BlockAsync());
     }
 
-    public override async Task<ChatRequestRes> Delete(SharedRequest request, ServerCallContext context)
+    public override async Task<ResultMsg> Delete(ChatRequestMsg request , ServerCallContext context)
     {
         return await ApplyAsync(request, async (model) => await _unitOfWork.DeleteAsync(model));
     }
 
-    public override async Task<ChatRequestRes> Unblock(SharedRequest request, ServerCallContext context)
+    public override async Task<ResultMsg> Unblock(ChatRequestMsg request , ServerCallContext context)
     {
         return await ApplyAsync(request, async (model) => await model.UnBlockAsync());
     }
@@ -45,7 +45,7 @@ public class ChatRequestHandler(IChatUOW _unitOfWork) : ChatRequestRPCs.ChatRequ
 
     //======================= privates
 
-    private async Task<ChatRequestRes> ApplyAsync(SharedRequest request, Func<ChatRequest, Task> actions)
+    private async Task<ResultMsg> ApplyAsync(ChatRequestMsg request , Func<ChatRequest, Task> actions)
     {
         var model = await FindRequestAsync(request.ChatRequestId.AsGuid());
         await actions.Invoke(model);
@@ -58,6 +58,6 @@ public class ChatRequestHandler(IChatUOW _unitOfWork) : ChatRequestRPCs.ChatRequ
             ?? throw new RpcException(Status.DefaultCancelled, $"The chatRequestId : <{chatRequestId}> not exist.");
         return model;
     }
-    private static ChatRequestRes DefaultResult => new() { IsSuccessful = true };
+    private static ResultMsg DefaultResult => new() { IsSuccessful = true };
 
 }
