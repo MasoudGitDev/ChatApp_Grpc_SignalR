a0544db0b1feda3149db6009ee7fb60036f38c7f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-27 15:17:58 +0330

    update : implement the ContactsQueries

--- Infrastructures/Infra.EFCore/Implementations/Chats/ContactQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Chats/ContactQueries.cs
@@ -1,18 +1,21 @@
 ﻿using Apps.Chats.Queries;
 using Domains.Chats.Contacts.Aggregate;
 using Infra.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.EFCore.Implementations.Chats;
 internal sealed class ContactQueries(AppDbContext _dbContext) : IContactQueries {
-    public Task<Contact?> FindAsync(Guid contactId) {
-        throw new NotImplementedException();
+    public async Task<Contact?> FindAsync(Guid contactId) {
+        return await _dbContext.Contacts.FirstOrDefaultAsync(x => x.Id == contactId);
     }
 
-    public Task<Contact?> FindByUserIdAsync(Guid userId) {
-        throw new NotImplementedException();
+    public async Task<Contact?> FindByUserIdAsync(Guid userId) {
+        return await _dbContext.Contacts.FirstOrDefaultAsync(x => x.ReceiverId == userId || x.RequesterId == userId);
     }
-
-    public Task<Contact?> IsInContactAsync(Guid userId1 , Guid userId2) {
-        throw new NotImplementedException();
+    public async Task<Contact?> IsInContactAsync(Guid userId1 , Guid userId2) {
+        return await _dbContext.Contacts
+             .Where(x => x.ReceiverId == userId1 || x.ReceiverId == userId2 )
+             .Where(x => x.RequesterId == userId1 || x.RequesterId == userId2)
+             .FirstOrDefaultAsync();
     }
 }
