de3712a8f34fd7c1bef5adffb40d58b6eac9d56a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-27 15:25:36 +0330

    create : imple Shared rpc

---
+++ Presentations/Server.ChatApp/GRPCHandlers/SharedModelsHandler.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Core;
+using Server.ChatApp.Protos;
+
+namespace Server.ChatApp.GRPCHandlers;
+public class SharedModelsHandler : SharedRpcs.SharedRpcsBase {
+    public override Task<Empty> shared(Empty request , ServerCallContext context) {
+        Console.WriteLine("SharedModelsHandler is running...");
+        return base.shared(request , context);
+    }
+}
