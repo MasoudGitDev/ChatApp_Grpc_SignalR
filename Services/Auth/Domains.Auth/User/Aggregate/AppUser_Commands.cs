f6fcf93a464c64318f55462d976a1b937e86c97b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-27 15:14:39 +0330

    create : partial AppUser (actually i separate it form base class)

---
+++ Services/Auth/Domains.Auth/User/Aggregate/AppUser_Commands.cs
@@ -0,0 +1,24 @@
+﻿using Domains.Auth.User.ValueObjects;
+using Shared.Server.Dtos;
+
+namespace Domains.Auth.User.Aggregate;
+public partial class AppUser {
+    public static AppUser Create(RegisterDto model , Guid? userId = null) {
+        return new AppUser() {
+            Id = userId ?? Guid.NewGuid() ,
+            UserName = model.UserName ,
+            Email = model.Email ,
+            EmailConfirmed = false ,
+            CreatedAt = DateTime.UtcNow ,
+        };
+    }
+    public static AppUser Empty => new() { Id = Guid.Empty , Email = "<invalid-email" , UserName = "<invalid-username>" };
+
+    public void Update(ProfileId profileId) {
+        if(UserName == profileId || Email == profileId) {
+            throw new Exception("Your Email or UserName can not be used as profileId!");
+        }
+        ProfileId = profileId;
+    }
+
+}
