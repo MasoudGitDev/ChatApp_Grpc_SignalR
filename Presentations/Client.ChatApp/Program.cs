5c2c87b22762e735e789bdf198a8fc3e33622189
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-27 15:20:47 +0330

    update : add SharedRPcs and change scoped service possitions in pipeline

--- Presentations/Client.ChatApp/Program.cs
+++ Presentations/Client.ChatApp/Program.cs
@@ -13,17 +13,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7001") });
-builder.Services.AddAuthorizationCore();
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<AuthenticationStateProvider , AuthStateProvider>();
 
 //register grpc channel
 var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+builder.Services.AddScoped(sp => {
+    return httpClient;
+});
 using var gRPCChannel = GrpcChannel.ForAddress("https://localhost:7001" , new GrpcChannelOptions {
     HttpClient = httpClient
 });
+
+builder.Services.AddScoped(x => gRPCChannel);
 
 builder.Services.AddScoped(services => {
     return new ChatMessageRPCs.ChatMessageRPCsClient(gRPCChannel);
@@ -36,5 +38,12 @@ builder.Services.AddScoped(service => {
 builder.Services.AddScoped(service => {
     return new ContactRPCs.ContactRPCsClient(gRPCChannel);
 });
+
+builder.Services.AddScoped(service => {
+    return new SharedRpcs.SharedRpcsClient(gRPCChannel);
+});
+builder.Services.AddAuthorizationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthenticationStateProvider , AuthStateProvider>();
 
 await builder.Build().RunAsync();
