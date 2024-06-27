26eedb5a220b3d3e7c2330e49a4525e61616498c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-27 15:15:26 +0330

    update : use partial and keep base class simple

--- Services/Auth/Domains.Auth/User/Aggregate/AppUser.cs
+++ Services/Auth/Domains.Auth/User/Aggregate/AppUser.cs
@@ -10,25 +10,6 @@ namespace Domains.Auth.User.Aggregate;
 public partial class AppUser : IdentityUser<Guid> {
 
     public DateTime CreatedAt { get; set; }
-    public ProfileId ProfileId { get; private set; } = string.Empty;
-
-}
-
-
-public partial class AppUser {
-    public static AppUser Create(RegisterDto model , Guid? userId = null) {
-        return new AppUser() {
-            Id = userId ?? Guid.NewGuid() ,
-            UserName = model.UserName ,
-            Email = model.Email ,
-            EmailConfirmed = false ,
-            CreatedAt = DateTime.UtcNow ,
-        };
-    }
-    public static AppUser Empty => new() { Id = Guid.Empty , Email = "<invalid-email" , UserName = "<invalid-username>" };
-
-    public void Update(ProfileId profileId) {
-        ProfileId = profileId;
-    }
+    public ProfileId ProfileId { get; private set; } = Guid.NewGuid().ToString().Replace("-" , "");
 
 }
