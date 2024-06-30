87e220bbf5e7bf75bf57101b5522f021d2bfa572
MasoudSh <Msh.Git.Dev@gmail.com>
2024-06-30 15:20:30 +0330

    create : to know what is the Type of Message (Error,Warning or Successful)

---
+++ Shared.Server/Constants/MessageType.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Server.Constants;  
+public record MessageType(string Name) {
+    public static MessageType Successful => new(nameof(Successful));
+    public static MessageType Warning => new(nameof(Warning));
+    public static MessageType Error => new(nameof(Error));
+
+    public static implicit operator string(MessageType messageType) => messageType.Name;
+}
