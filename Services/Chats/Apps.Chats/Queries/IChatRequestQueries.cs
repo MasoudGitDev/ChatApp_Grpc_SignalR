c942510dab1b929af5b75ab741b82f4e424421bc
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-01 14:55:06 +0330

    update :add FindSameRequestAsync

--- Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
+++ Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
@@ -1,8 +1,7 @@
-﻿using Domains.Auth.User.Aggregate;
-using Domains.Chats.Requests.Aggregate;
+﻿using Domains.Chats.Requests.Aggregate;
 
-namespace Apps.Chats.Queries {
-   public interface IChatRequestQueries {
-        Task<ChatRequest?> FindByIdAsync(Guid chatRequestId);
-    }
+namespace Apps.Chats.Queries;
+public interface IChatRequestQueries {
+    Task<ChatRequest?> FindByIdAsync(Guid chatRequestId);
+    Task<ChatRequest?> FindSameRequestAsync(Guid userId1 , Guid userId2);
 }
