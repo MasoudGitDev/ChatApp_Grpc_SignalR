f590cdfb0cf81c9b626e80ab6a2f283fdb0203a8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-01 14:30:10 +0330

    update : change names and improve the all methods

--- Presentations/Client.ChatApp/Pages/Dashboard/Profile.razor.cs
+++ Presentations/Client.ChatApp/Pages/Dashboard/Profile.razor.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Shared.Server.Constants;
+using Shared.Server.Constants.View;
+
 namespace Client.ChatApp.Pages.Dashboard;
 
 public class ProfileViewHandler : ComponentBase {
@@ -24,46 +26,47 @@ public class ProfileViewHandler : ComponentBase {
     private Task<AuthenticationState> AuthState { get; set; } = null!;
 
     //========================== Use In View
-    protected List<ErrorInfo> Errors = [];
-    protected string IsAnyError = "";   
+    protected List<MessageInfo> Messages = [];
+    protected bool IsAnyError = false;   
     protected BoolType IsMe = BoolType.None;
-    protected bool IsButtonHidden = true;
-    protected string ButtonName => ExistInContacts.IsYes() ? "Dialog" : "Request";
+    protected bool CanShowButton = false;
+    protected string ButtonName = string.Empty;
     //================================ private fields
-    private BoolType ExistInContacts = BoolType.None;
-    private string UserId = String.Empty;
     private string ReceiverId =  String.Empty;
-    private BoolType IsValidUserName = BoolType.None;
     //====================== protected Methods
     protected override async Task OnInitializedAsync() {
         try {
+            Messages.Clear();
             var result = await ContactService.IsInContactsAsync(new ContactMsg() { ProfileId = ProfileId });      
-            UserId = await GetIdByClaimAsync();
             CheckContactResult(result);            
         }
         catch(Exception ex) {
             Console.WriteLine("OnInitializedAsync : " + ex.Message);
-            Errors.Add(new ErrorInfo() { Code = "Loading-Error" , Description = "The page not load Completely." });
+            Messages.Add(new MessageInfo() { Code = "Loading-Error" , Description = "The page not load Completely." , Type = MessageType.Error });
         }
     }
     protected async Task OnButtonClick() {
-        if(ButtonName == "Dialog" && IsMe.IsYes()) {
+        if(ButtonName == ProfileViewConstants.DialogBtn && IsMe.IsYes()) {
             NavManager.NavigateTo("/Chats");
             return;
         }
-        if(ButtonName == "Dialog") {
+        if(ButtonName == ProfileViewConstants.DialogBtn) {
             NavManager.NavigateTo("/Chats");
             return;
         }
-        if(ButtonName == "Request") {
+        if(ButtonName == ProfileViewConstants.RequestBtn) {
             await SendChatRequestAsync();
             return;
         }
+        if(ButtonName == ProfileViewConstants.ConfirmBtn) {
+            
+            return;
+        }
     }
-    protected void CloseNotification(ErrorInfo model) {
-        Errors.Remove(model);
-        if(Errors.Count <= 0) {
-            IsAnyError = "";
+    protected void CloseNotification(MessageInfo model) {
+        Messages.Remove(model);
+        if(Messages.Count <= 0) {
+            IsAnyError = false;
         }
     }
 
@@ -72,39 +75,19 @@ public class ProfileViewHandler : ComponentBase {
         => ( await AuthState ).User.Claims.Where(x => x.Type == "UserIdentifier").FirstOrDefault()?.Value ?? string.Empty;
     private void CheckContactResult(ContactResult result) {
         foreach(var message in result.Messages) {
-            Errors.Add(message);
-            if(message.Code == "Invalid-ProfileId") {
-                IsValidUserName = BoolType.No;
-            }
-            if(message.Code == "NotExist") {
-                ExistInContacts = BoolType.No;
-            }
-            if(message.Code == "NotPossible") {
-                IsMe = BoolType.Yes;
-            }
+            Messages.Add(message);
+            (ButtonName,CanShowButton) = ProfileViewConstants.ApplyCodeResult(message.Code);
         }
-        if(IsValidUserName.IsNotNo()) {
-            IsValidUserName = BoolType.Yes;
-        }
-        if(ExistInContacts.IsNotNo()) {
-            ExistInContacts = BoolType.Yes;
-        }
-        IsButtonHidden = ( IsValidUserName.IsYes() && IsMe.IsNotYes() );
-        IsAnyError = Errors.Any() ? "hidden" : "";
+        IsAnyError = Messages.Count > 0;
         ReceiverId = result.ContactInfo.UserId;
-    }  
-    private async Task SendChatRequestAsync() {
-        Errors.Clear();
-        var result = await RequestService.RequestAsync(new UsersConnection()
-        {
-            RequesterId = UserId ,
-            ReceiverId = ReceiverId
-        });
-        if(!result.IsSuccessful) {
-            Errors.AddRange(result.Messages);
-        }
     }
-
-    //=================================
-  
+    private async Task SendChatRequestAsync() {
+        if(Errors.Count > 0) {
+            Messages.Add(new MessageInfo() { Code = "NotPossible" , Description = "Because of other errors" , Type = MessageType.Error });
+            return;
+        }
+        var result = await RequestService.RequestAsync(new UserMsg() { UserId = ReceiverId });
+        Messages.AddRange(result.Messages);
+    }
+    private List<MessageInfo> Errors => Messages.Where(x=> x.Type == MessageType.Error).Select(x=>x).ToList(); 
 }
