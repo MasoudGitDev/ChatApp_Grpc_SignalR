ecd385d3eb6883ece87bc92d42e7708f9a39d788
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-01 14:55:41 +0330

    create : some shared methods

---
+++ Presentations/Server.ChatApp/GRPCHandlers/SharedMethods.cs
@@ -0,0 +1,17 @@
+﻿using Apps.Chats.UnitOfWorks;
+using Domains.Auth.User.Aggregate;
+using Grpc.Core;
+
+namespace Server.ChatApp.GRPCHandlers;  
+public static class SharedMethods {
+    public static async Task<AppUser> GetUserAsync(ServerCallContext context , IChatUOW unitOfWork) {
+        var user = context.GetHttpContext().User;
+        if(user is null || user.Identity is null || !user.Identity.IsAuthenticated) {
+            throw new RpcException(Status.DefaultCancelled , "You are not authenticated.");
+        }
+        return await unitOfWork.Queries.Users.FindByUserNameAsync(user.Identity.Name ?? String.Empty)
+            ?? throw new RpcException(Status.DefaultCancelled , "Invalid-User");
+    }
+    public static async Task<Guid> GetUserIdAsync(ServerCallContext ctx , IChatUOW unitOfWork) => ( await GetUserAsync(ctx,unitOfWork) ).Id;
+    //======================
+}
