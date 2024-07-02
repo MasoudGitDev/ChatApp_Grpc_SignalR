1c5a99b4e564dc145f8b61e17026f3b6002f313e
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-02 20:16:56 +0330

    update : change names  , add Type prop , add implicit operator , and new Create Method

--- Shared.Server/Models/CodeMessage.cs
+++ Shared.Server/Models/CodeMessage.cs
@@ -1,10 +1,16 @@
-﻿namespace Shared.Server.Models;
-public record class CodeMessage {
-    public CodeMessage()
+﻿using Shared.Server.Constants;
+
+namespace Shared.Server.Models;
+public record class MessageDescription {
+    public MessageDescription()
     {
         
     }
     public string Code { get; private set; } = "<empty-code>";
-    public string Message { get; private set; } = "<empty-message>";
-    public static CodeMessage Create(string code , string message) => new() { Code = code , Message = message };
+    public string Description { get; private set; } = "<empty-description>";
+    public MessageType Type { get; set; } = MessageType.None;
+    public static MessageDescription Create(string code , string message) => new() { Code = code , Description = message };
+    public static MessageDescription Create(string code , string message, MessageType type) => new() { Code = code , Description = message , Type = type };
+
+    public static implicit operator string(MessageDescription message) => message.Code;
 }
