176ea0ea2e95d9180e0de7936b01711e708278db
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-02 20:33:01 +0330

    create : some shared static methods

---
+++ Presentations/Client.ChatApp/Constants/SharedViewCodes.cs
@@ -0,0 +1,12 @@
+﻿using Client.ChatApp.Protos;
+
+namespace Shared.Server.Constants.View;  
+public class SharedViewCodes {
+    public static List<MessageInfo> GetErrors(List<MessageInfo> messages)
+       => messages.Where(x => x.Type == MessageType.Error).Select(x => x).ToList();
+
+    // write better description...
+    public static MessageInfo NotPossible => new() { Code = "NotPossible" , Description ="Some things is wrong." , Type = MessageType.Error };
+    public static MessageInfo NotCompletedLoading => new() { Code = "NotCompletedLoading" , Description = "The page is not loading completely." , Type = MessageType.Error };
+
+}
