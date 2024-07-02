f97d22e170351d2ebdc9a7b7c4b81936651e8b17
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-02 20:33:43 +0330

    update : add FindSameRequestAsync method

--- Infrastructures/Infra.EFCore/Implementations/Chats/ChatRequestQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Chats/ChatRequestQueries.cs
@@ -8,4 +8,11 @@ internal sealed class ChatRequestQueries(AppDbContext _dbContext) : IChatRequest
     public async Task<ChatRequest?> FindByIdAsync(Guid chatRequestId) {
         return await _dbContext.ChatRequests.FirstOrDefaultAsync(x => x.Id == chatRequestId);
     }
+
+    public async Task<ChatRequest?> FindSameRequestAsync(Guid userId1 , Guid userId2) {
+        return await _dbContext.ChatRequests
+            .Where(x => x.RequesterId == userId1 || x.RequesterId == userId2)
+            .Where(x => x.ReceiverId == userId1 || x.ReceiverId == userId2)
+            .FirstOrDefaultAsync();
+    }
 }
