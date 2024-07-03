aff0a983a8b6fa85ca2b9bf65e64803b6ba944cd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-03 14:46:13 +0330

    update : add and implements GetReceiveRequestsAsync and  GetSendRequestsAsync

--- Infrastructures/Infra.EFCore/Implementations/Chats/ChatRequestQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Chats/ChatRequestQueries.cs
@@ -2,6 +2,7 @@
 using Domains.Chats.Requests.Aggregate;
 using Infra.EFCore.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Shared.Server.Extensions;
 
 namespace Infra.EFCore.Implementations.Chats;
 internal sealed class ChatRequestQueries(AppDbContext _dbContext) : IChatRequestQueries {
@@ -14,5 +15,17 @@ internal sealed class ChatRequestQueries(AppDbContext _dbContext) : IChatRequest
             .Where(x => x.RequesterId == userId1 || x.RequesterId == userId2)
             .Where(x => x.ReceiverId == userId1 || x.ReceiverId == userId2)
             .FirstOrDefaultAsync();
+    }
+
+    public async Task<LinkedList<ChatRequest>> GetReceiveRequestsAsync(Guid receiverId) {
+        return await _dbContext.ChatRequests
+            .Where(x => x.ReceiverId == receiverId)
+            .ToLinkedListAsync();
+    }
+
+    public async Task<LinkedList<ChatRequest>> GetSendRequestsAsync(Guid requesterId) {
+        return await _dbContext.ChatRequests
+        .Where(x => x.RequesterId == requesterId)
+        .ToLinkedListAsync();
     }
 }
--- Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
+++ Services/Chats/Apps.Chats/Queries/IChatRequestQueries.cs
@@ -4,4 +4,6 @@ namespace Apps.Chats.Queries;
 public interface IChatRequestQueries {
     Task<ChatRequest?> FindByIdAsync(Guid chatRequestId);
     Task<ChatRequest?> FindSameRequestAsync(Guid userId1 , Guid userId2);
+    Task<LinkedList<ChatRequest>> GetReceiveRequestsAsync(Guid receiverId);
+    Task<LinkedList<ChatRequest>> GetSendRequestsAsync(Guid requesterId);
 }
