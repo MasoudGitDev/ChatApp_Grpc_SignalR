c5a3b4441f9f3e3dec160432e5bb9d533e73ae93
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-03 14:49:56 +0330

    create : implementaion of chat_request_queries.proto

---
+++ Presentations/Server.ChatApp/GRPCHandlers/ChatRequestQueriesHandler.cs
@@ -0,0 +1,15 @@
+﻿using Apps.Chats.UnitOfWorks;
+using Grpc.Core;
+using Server.ChatApp.Protos;
+
+namespace Server.ChatApp.GRPCHandlers;
+
+internal sealed class ChatRequestQueriesHandler(IChatUOW _unitOfWork) : ChatRequestQueryRPCs.ChatRequestQueryRPCsBase {
+    public override Task<CRQResult> GetReceiveRequests(UserMsg request , ServerCallContext context) {
+        return base.GetReceiveRequests(request , context);
+    }
+
+    public override Task<CRQResult> GetSendRequests(UserMsg request , ServerCallContext context) {
+        return base.GetSendRequests(request , context);
+    }
+}
