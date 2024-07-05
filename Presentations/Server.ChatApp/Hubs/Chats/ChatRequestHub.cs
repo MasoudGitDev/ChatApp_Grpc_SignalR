a40a1222e4e6adf522ddc8d58f17042b196afc4d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-05 11:47:06 +0330

    create : ChatRequest Hub

---
+++ Presentations/Server.ChatApp/Hubs/Chats/ChatRequestHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Shared.Server.Models.Results;
+
+namespace Server.ChatApp.Hubs.Chats;
+public class ChatRequestHub : Hub {
+
+    public async Task Request(ChatRequestItem item) {
+        await Clients.All.SendAsync("GetReceiveRequests" , item , new CancellationToken());
+        await Clients.User(MyId).SendAsync("GetSendRequests" , item , new CancellationToken());
+    }
+    private string MyId => SharedHubMethods.GetMyIdByClaims(Context).ToString();
+}
