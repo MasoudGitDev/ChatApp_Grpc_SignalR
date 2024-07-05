7a2b301e3eb733bd305b207f001c36c4f6b90f00
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-05 11:48:36 +0330

    create : add AsRepeatedFields extension method

---
+++ Presentations/Server.ChatApp/Extensions/GrpcExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Google.Protobuf.Collections;
+using Mapster;
+
+namespace Server.ChatApp.Extensions;
+
+public static class GrpcExtensions {
+
+    public static RepeatedField<T> AsRepeatedFields<T, U>(this LinkedList<U> items)
+        where T : class, new()
+        where U : class, new() {
+        RepeatedField<T> result = [];
+        foreach(var item in items) {
+            result.Add(item.Adapt<T>());
+        }
+        return result;
+    }
+
+}
