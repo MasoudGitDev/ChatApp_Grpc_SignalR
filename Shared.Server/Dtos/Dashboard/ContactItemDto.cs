a6e45aeb39e8d40b9c2e8cfec00b8052bb533867
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-05 11:45:00 +0330

    Create : ContactItem Dto

---
+++ Shared.Server/Dtos/Dashboard/ContactItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.Server.Dtos.Dashboard;
+public record ContactItemDto(
+    Guid ContactId ,
+    Guid UserId ,
+    string DisplayName ,
+    DateTime ContactedAt ,
+    string ImageUrl
+) {
+    public ContactItemDto():this(Guid.Empty,Guid.Empty,"<empty>" , DateTime.UtcNow , "<empty>")
+    {
+        
+    }
+};
