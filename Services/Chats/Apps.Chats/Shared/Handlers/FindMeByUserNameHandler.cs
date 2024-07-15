b7ad037f89686240adb3f431f20df9a2407dc96e
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-15 23:13:59 +0330

    create : FindMeByUserNameRequest Model and its mediatR handler

---
+++ Services/Chats/Apps.Chats/Shared/Handlers/FindMeByUserNameHandler.cs
@@ -0,0 +1,11 @@
+﻿using Apps.Chats.Shared.Models;
+using Apps.Chats.UnitOfWorks;
+using Domains.Auth.User.Aggregate;
+using MediatR;
+
+namespace Apps.Chats.Shared.Handlers;
+internal sealed class FindMeByUserNameHandler(IChatUOW _unitOfWork) : IRequestHandler<FindMeByUserNameRequest , AppUser> {
+    public async Task<AppUser> Handle(FindMeByUserNameRequest request , CancellationToken cancellationToken) {
+        return await _unitOfWork.Queries.Users.FindByUserNameAsync(request.UserName) ?? AppUser.Empty;
+    }
+}
---
+++ Services/Chats/Apps.Chats/Shared/Models/FindMeByUserNameRequest.cs
@@ -0,0 +1,5 @@
+﻿using Domains.Auth.User.Aggregate;
+using MediatR;
+
+namespace Apps.Chats.Shared.Models;
+public record FindMeByUserNameRequest(string UserName) : IRequest<AppUser>;
