af185ad5c21a55f0f8d5256e249b8f12f10ef5d0
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-15 23:09:44 +0330

    update : remove unused static members

--- Shared.Server/Models/Results/Result.cs
+++ Shared.Server/Models/Results/Result.cs
@@ -1,7 +1,2 @@
 ﻿namespace Shared.Server.Models.Results;
-public record class Result(string Code, string Message)
-{
-    public static Result Ok => new("Ok", "Every think is ok.");
-    public static Result Cancel => new("Cancel", "The operations was canceled.");
-    public static Result Error(string message) => new("Error", "Some thing is wrong.");
-}
+public record class Result(string Code, string Message);
