b44d5eff12d264e58ba497fe1496a3067c15e11b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-15 23:07:49 +0330

    refactor : change return type from Result to ResultStatus

--- Shared.Server/Models/Results/ChatItemResult.cs
+++ Shared.Server/Models/Results/ChatItemResult.cs
@@ -1,8 +1,20 @@
 ﻿namespace Shared.Server.Models.Results;
 
 public record class ChatItemResult(string Code , string Message) {
-    public static Result Created => new("Ok" , "The new chat was created successfully.");
-    public static Result SameId => new("SameId" , "You can not chat with yourself!");
-    public static Result Duplicate => new("Duplicate" , "There is a chat between you and the other person!");
-    public static Result NotFound(string message) => new("NotFound" , message);
+    public static ResultStatus SameId
+        => new(false , [MessageDescription.Create(nameof(SameId) , "You can not chat with yourself!")]);
+
+    public static ResultStatus Founded
+        => new(false , [MessageDescription.Create(nameof(Founded) , "There is a chat between you and the other person!")]);
+
+    public static ResultStatus NotFound(string message)
+        => new(false , [MessageDescription.Create(nameof(Created) , message)]);
+
+    public static ResultStatus Removed(Guid id)
+        => new(true , [MessageDescription.Create(nameof(Removed) , $"The record with id :<{id}> is removed successfully.")]);
+
+    public static ResultStatus Created
+        => new(true , [MessageDescription.Create(nameof(Created) , "The new record is created successfully.")]);
+
+
 }
