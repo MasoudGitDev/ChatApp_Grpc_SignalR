b5968b2d33d62591f5dfdec78e8463bd6e2bed23
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-15 23:26:49 +0330

    create : CreateRequest and its mediatR Handler to handle ChatItem creation

---
+++ Services/Chats/Apps.Chats/ChatItems/Commands/Handlers/CreateHandler.cs
@@ -0,0 +1,33 @@
+﻿using Apps.Chats.ChatItems.Commands.Model;
+using Apps.Chats.UnitOfWorks;
+using Domains.Chats.Item.Aggregate;
+using MediatR;
+using Shared.Server.Models.Results;
+
+namespace Apps.Chats.ChatItems.Commands.Handlers;
+internal sealed class CreateHandler(IChatUOW _unitOfWork) : IRequestHandler<CreateRequest , ResultStatus> {
+    public async Task<ResultStatus> Handle(CreateRequest request , CancellationToken cancellationToken) {
+        var (requesterId, receiverId) = request;
+
+        // same id can not have chat!
+        if(requesterId == receiverId) {
+            return ChatItemResult.SameId;
+        }
+
+        //  The Receiver user must exist in the db.
+        var receiverUser = await _unitOfWork.Queries.Users.FindByIdAsync(receiverId);
+        if(receiverUser is null) {
+            return ChatItemResult.NotFound($"The {nameof(receiverId)} : <{receiverId}> is invalid.");
+        }
+
+        // ensure each requester-receiver users just have one chatItem row in db that related to each other!
+        var isAnyChat = await _unitOfWork.Queries.ChatItems.HaveAnyChatItem(requesterId, receiverId);
+        if(isAnyChat) {
+            return ChatItemResult.Founded;
+        }
+
+        await _unitOfWork.CreateAsync(ChatItem.Create(requesterId , receiverId));
+        await _unitOfWork.SaveChangeAsync();
+        return ChatItemResult.Created;
+    }
+}
---
+++ Services/Chats/Apps.Chats/ChatItems/Commands/Model/CreateRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Shared.Server.Models.Results;
+
+namespace Apps.Chats.ChatItems.Commands.Model;
+public sealed record CreateRequest(Guid RequesterId , Guid ReceiverId) : IRequest<ResultStatus> {
+    
+}
