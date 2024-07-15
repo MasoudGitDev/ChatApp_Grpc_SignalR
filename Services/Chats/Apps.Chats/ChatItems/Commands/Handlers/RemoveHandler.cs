0a48f696758e703fddf267c7ce7fb2f71e11530b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-15 23:27:44 +0330

    create : RemoveRequest and its MediatR handler to handle ChatItem Deletion

---
+++ Services/Chats/Apps.Chats/ChatItems/Commands/Handlers/RemoveHandler.cs
@@ -0,0 +1,18 @@
+﻿using Apps.Chats.ChatItems.Commands.Model;
+using Apps.Chats.UnitOfWorks;
+using MediatR;
+using Shared.Server.Models.Results;
+
+namespace Apps.Chats.ChatItems.Commands.Handlers;
+internal sealed class RemoveHandler(IChatUOW _unitOfWork) : IRequestHandler<RemoveRequest , ResultStatus> {
+    public async Task<ResultStatus> Handle(RemoveRequest request , CancellationToken cancellationToken) {
+        var chatItemId = request.ChatItemId;
+        var chatItem = await _unitOfWork.Queries.ChatItems.FindByIdAsync(chatItemId);
+        if(chatItem is null) {
+            return ChatItemResult.NotFound($"The {nameof(chatItemId)} : <{chatItemId}> not found.");
+        }
+        await _unitOfWork.DeleteAsync(chatItem);
+        await _unitOfWork.SaveChangeAsync();
+        return ChatItemResult.Removed(chatItemId);
+    }
+}
---
+++ Services/Chats/Apps.Chats/ChatItems/Commands/Model/RemoveRequest.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+using Shared.Server.Models.Results;
+
+namespace Apps.Chats.ChatItems.Commands.Model;
+public record RemoveRequest(Guid ChatItemId) : IRequest<ResultStatus>;
