515bf24e933a3afbed69689c0bafe5dca52e7a17
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-17 08:37:51 +0330

    create : to handle successful result status

---
+++ Shared.Server/Models/Results/SuccessResults.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Server.Models.Results;
+
+public record class SuccessResults(string Code , string Message) {
+    public static ResultStatus Removed(Guid id)
+        => new(true , [MessageDescription.Create(nameof(Removed) , $"The record with id :<{id}> is removed successfully.")]);
+
+    public static ResultStatus Created
+        => new(true , [MessageDescription.Create(nameof(Created) , "The new record is created successfully.")]);
+
+    public static ResultStatus Ok(string message)
+      => new(true , [MessageDescription.Create(nameof(Ok) , message)]);
+}
