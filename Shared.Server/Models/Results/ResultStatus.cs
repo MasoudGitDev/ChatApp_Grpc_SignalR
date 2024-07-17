1337a8a466f6020afee6769d64d98dcd9a6baaaa
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-17 08:37:16 +0330

    update : add generic ResultStatus and IResultStatus to handle result status better

--- Shared.Server/Models/Results/ResultStatus.cs
+++ Shared.Server/Models/Results/ResultStatus.cs
@@ -1,3 +1,10 @@
 ﻿namespace Shared.Server.Models.Results;
 
-public record class ResultStatus(bool IsSuccessful , List<MessageDescription> Messages);
+public interface IResultStatus {
+    bool IsSuccessful { get; init; } 
+    List<MessageDescription> Messages { get; init; }
+}
+
+public record class ResultStatus(bool IsSuccessful , List<MessageDescription> Messages):IResultStatus;
+public record class ResultStatus<T>(bool IsSuccessful , List<MessageDescription> Messages, T? Model):IResultStatus;
+
