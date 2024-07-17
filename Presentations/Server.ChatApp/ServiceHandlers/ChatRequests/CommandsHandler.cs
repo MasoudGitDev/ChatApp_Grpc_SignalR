0fa72cb7135fd878c5cac119f3f03103fc471991
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-17 08:32:48 +0330

    refactor : write handlers with medaitR ,change the possition, change names

---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatRequests/CommandsHandler.cs
@@ -0,0 +1,46 @@
+﻿using Apps.Chats.ChatRequests.Commands.Models;
+using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+using Shared.Server.Extensions;
+
+namespace Server.ChatApp.ServiceHandlers.ChatRequests;
+
+/// <summary>
+/// A ChatRequest Commands Handler
+/// </summary>
+[Authorize]
+internal class CommandsHandler(IMediator _mediator) : ChatRequestCommandsRPCs.ChatRequestCommandsRPCsBase {
+
+    private readonly CancellationToken _cancellationToken = new();
+
+    public override async Task<ResultMsg> Accept(ChatRequestMsg request , ServerCallContext context)
+        => ( await _mediator.Send(new AcceptRequest(
+            request.ChatRequestId.AsGuid() , await GetMyIdAsync(context)) , _cancellationToken) )
+        .AsCommonResult();
+
+    public override async Task<ResultMsg> Block(ChatRequestMsg request , ServerCallContext context)
+        => ( await _mediator.Send(new BlockRequest(
+            request.ChatRequestId.AsGuid() , await GetMyIdAsync(context)) , _cancellationToken) )
+        .AsCommonResult();
+
+    public override async Task<ResultMsg> Delete(ChatRequestMsg request , ServerCallContext context)
+        => ( await _mediator.Send(new BlockRequest(
+            request.ChatRequestId.AsGuid() , await GetMyIdAsync(context)) , _cancellationToken) )
+        .AsCommonResult();
+
+    public override async Task<ResultMsg> Request(PersonMsg request , ServerCallContext context)
+        => ( await _mediator.Send(new CreateRequest(
+            await GetMyIdAsync(context) , request.Id.AsGuid()) , _cancellationToken) )
+        .AsCommonResult();
+
+    public override async Task<ResultMsg> Unblock(ChatRequestMsg request , ServerCallContext context)
+        => ( await _mediator.Send(new UnblockRequest(
+             request.ChatRequestId.AsGuid() , await GetMyIdAsync(context)) , _cancellationToken) )
+        .AsCommonResult();
+
+    private async Task<Guid> GetMyIdAsync(ServerCallContext ctx)
+        => await SharedMethods.GetMyIdAsync(ctx , _mediator);
+}
---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatRequests/QueriesHandler.cs
@@ -0,0 +1,19 @@
+﻿using Apps.Chats.ChatRequests.Queries;
+using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+
+namespace Server.ChatApp.ServiceHandlers.ChatRequests;
+
+[Authorize]
+internal class QueriesHandler(IMediator _mediator) : ChatRequestQueryRPCs.ChatRequestQueryRPCsBase {
+    public override async Task<CRQResult> GetReceiveRequests(Empty request , ServerCallContext context)
+        => ( await _mediator.Send(new GetReceiveRequests(await SharedMethods.GetMyIdAsync(context , _mediator))) )
+        .AsChatRequestQueriesResult();
+
+    public override async Task<CRQResult> GetSendRequests(Empty request , ServerCallContext context)
+        => ( await _mediator.Send(new GetSendRequests(await SharedMethods.GetMyIdAsync(context , _mediator))) )
+        .AsChatRequestQueriesResult();
+}
