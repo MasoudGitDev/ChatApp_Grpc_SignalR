8466314818553993eeb50fe937c3544eadd36af4
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 16:11:10 +0330

    Create : Accept Command (model and handler) to handle Accept Chat Request

---
+++ Services/Chats/Apps.Chats/ChatRequests/Commands/Accept.cs
@@ -0,0 +1,25 @@
+﻿using Domains.Chats.Contacts.Aggregate;
+using MediatR;
+using Shared.Server.Extensions;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Chats.ChatRequests.Commands;
+
+// ChatRequests Accept Model
+public sealed record Accept(Guid ChatRequestId , Guid MyId) : IRequest<ResultStatus> {
+    public static Accept New(Guid ChatRequestId , Guid MyId) => new(ChatRequestId , MyId);
+}
+
+
+// ChatRequests Accept Handler
+internal sealed class AcceptHandler(IChatUOW _unitOfWork)
+    : ChatRequestHandler<Accept , ResultStatus>(_unitOfWork.ThrowIfNull("The IChatUOW can not be null!")) {
+    public override async Task<ResultStatus> Handle(Accept request , CancellationToken cancellationToken)
+      => await DoAsync(request.ChatRequestId , async (model) => {
+          await _unitOfWork.DeleteAsync(model);
+          await _unitOfWork.CreateAsync(Contact.Create(model.RequesterId , model.ReceiverId));
+      } , okMessage);
+
+    private const string okMessage = "The request has been accepted successfully.";
+}
