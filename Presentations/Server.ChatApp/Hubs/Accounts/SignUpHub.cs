8e94f90f5f2324a27f8e4746a88117ea85a5c2a1
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 16:36:38 +0330

    create : use as a signalr hub between home page and signUp page

---
+++ Presentations/Server.ChatApp/Hubs/Accounts/SignUpHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Shared.Server.Dtos.User;
+
+namespace Server.ChatApp.Hubs.Accounts;
+
+public class SignUpHub :Hub{
+    public async Task SendNewUser(UserHomeDto user) {
+        await Clients.All.SendAsync("GetNewUser" , user);
+    }
+}
