1a7d9501db48665f409f099e5382aeab1795f572
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 16:21:49 +0330

    update : add PageSize , TotalItems and change access Modifiers  ,

--- Presentations/Client.ChatApp/Pages/Dashboard/Contacts.razor.cs
+++ Presentations/Client.ChatApp/Pages/Dashboard/Contacts.razor.cs
@@ -1,4 +1,5 @@
-﻿using Client.ChatApp.Protos;
+﻿using Client.ChatApp.Pages.Features;
+using Client.ChatApp.Protos;
 using Google.Protobuf.Collections;
 using Microsoft.AspNetCore.Components;
 
@@ -13,22 +14,42 @@ public class ContactsViewHandler : ComponentBase {
     [Inject]
     private ContactRPCs.ContactRPCsClient ContactService { get; set; } = null!;
 
+    //private
+    private uint _currentPage = 1;
+
+
     //==============protected 
-    protected RepeatedField<ContactItem> _contacts = new();
+    protected uint PageSize = 5;
+    protected uint TotalItems = 1;
+
+    protected RepeatedField<ContactItem> _contacts = [];
 
     protected async Task OnRemoveItem(ContactItem contact) {
         _contacts.Remove(contact);
         await ContactService.RemoveAsync(new RowMsg() { RowId = contact.ContactId.ToString() });
+    }
+    protected void GoToChat(ContactItem item) {
+        NavManager.NavigateTo("/Chats");
     }
 
     protected void GoToDialog(ContactItem contact) {
         NavManager.NavigateTo("/Chats");
     }
 
+   
     protected override async Task OnInitializedAsync() {
         _contacts = ( await ContactService.GetContactsAsync(new Empty() { }) ).Items;
+        TotalItems = (uint) _contacts.Count;
     }
 
     //====================
+
+    protected Pagination pagination  = new();
+
+    protected async Task NotifyOnChangePage((uint pageSize , uint currentPage) info) {
+        _currentPage = info.currentPage;
+        PageSize = info.pageSize;
+        await InvokeAsync(StateHasChanged);
+    }
 
 }
