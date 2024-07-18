6fc047cae71e9ace1f653988c1db27f628f8fb19
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 16:57:52 +0330

    create : provide basic code for fetch GetUser to use as List of HomeUsersDTO

---
+++ Presentations/Server.ChatApp/ServiceHandlers/Users/QueriesHandler.cs
@@ -0,0 +1,29 @@
+﻿using Apps.Auth.Users.Queries;
+using Grpc.Core;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Protos;
+using Server.ChatApp.Protos.Users;
+using Shared.Server.Dtos.User;
+using Shared.Server.Models.Results;
+
+namespace Server.ChatApp.ServiceHandlers.Users;
+
+[Authorize]
+public class QueriesHandler(IMediator _mediator) : UserQeriesRPCs.UserQeriesRPCsBase {
+
+
+    /// <summary>
+    ///  usage in home page
+    /// </summary>
+    public override async Task GetUsers(Empty request , IServerStreamWriter<UserMemberMsg> responseStream , ServerCallContext context) {
+        ResultStatus<List<UserHomeDto>> result = await _mediator.Send(GetHomeUsers.New());
+        if(!result.IsSuccessful) {
+            throw new RpcException(Status.DefaultCancelled);
+        }
+        foreach(var user in result.Model!) {
+            await responseStream.WriteAsync(user.Adapt<UserMemberMsg>());
+        }        
+    }
+}
