42651197d6b2434aa4522b3514dd574379276626
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 14:37:20 +0330

    remove : unused queries

--- Infrastructures/Infra.EFCore/Implementations/Accounts/AccountQueries.cs
+++
@@ -1,6 +0,0 @@
-﻿using Apps.Auth.Queries;
-
-namespace Infra.EFCore.Implementations.Accounts;
-internal class AccountQueries(IUserQueries _userQueries) : IAccountQueries {
-    public IUserQueries User => _userQueries;
-}
--- Services/Auth/Apps.Auth/Queries/IAccountQueries.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Apps.Auth.Queries;
-public class IAccountQueries {
-    public IUserQueries User { get; }
-}
