13c9950b974fa619f6469407286eaaed479c2545
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 14:38:52 +0330

    create : User Queries To handle user's queries with respect CQRS

---
+++ Services/Auth/Domains.Auth/User/Queries/IUserQueries.cs
@@ -0,0 +1,11 @@
+﻿using Domains.Auth.User.Aggregate;
+using Domains.Auth.User.ValueObjects;
+
+namespace Domains.Auth.Queries;
+public interface IUserQueries {
+    Task<AppUser?> FindByUserNameAsync(string username);
+    Task<AppUser?> FindByIdAsync(Guid userId);
+    Task<AppUser?> FindByProfileIdAsync(ProfileId profileId);
+    Task<List<AppUser>> GetUsersAsync();
+}
+
