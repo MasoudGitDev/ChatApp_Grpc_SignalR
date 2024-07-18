54fbe75a0786d21ae82b1002af982a52036455f9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 14:31:06 +0330

    update : add generic Respons to handle ResultStatus<T>

--- Shared.Server/Models/Results/SuccessResults.cs
+++ Shared.Server/Models/Results/SuccessResults.cs
@@ -1,6 +1,7 @@
 ﻿namespace Shared.Server.Models.Results;
 
 public record class SuccessResults(string Code , string Message) {
+    // for commands
     public static ResultStatus Removed(Guid id)
         => new(true , [MessageDescription.Create(nameof(Removed) , $"The record with id :<{id}> is removed successfully.")]);
 
@@ -8,5 +9,9 @@ public record class SuccessResults(string Code , string Message) {
         => new(true , [MessageDescription.Create(nameof(Created) , "The new record is created successfully.")]);
 
     public static ResultStatus Ok(string message)
-      => new(true , [MessageDescription.Create(nameof(Ok) , message)]);
+        => new(true , [MessageDescription.Create(nameof(Ok) , message)]);
+
+    // for queries
+    public static ResultStatus<T> Ok<T>(T data)
+        => new(true , [],data);
 }
