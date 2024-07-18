818822c1cc83cf40342142efb8f39bb9a30eafa7
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 16:32:34 +0330

    Create : To Fetch All Send Chat Request By Me

---
+++ Services/Chats/Apps.Chats/ChatRequests/Queries/GetSendRequestsHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+using UnitOfWorks.Extensions;
+
+namespace Apps.Chats.ChatRequests.Queries;
+
+public record GetSendRequests(Guid MyId) : IRequest<ResultStatus<List<ChatRequestItem>>> {
+    public static GetSendRequests New(Guid MyId) => new(MyId);
+}
+
+//============ Handler
+internal sealed class GetSendRequestsHandler(IChatUOW _unitOfWork)
+    : ChatRequestHandler<GetSendRequests , ResultStatus<List<ChatRequestItem>>>(_unitOfWork.HasValue()) {
+    public override async Task<ResultStatus<List<ChatRequestItem>>> Handle(GetSendRequests request ,
+        CancellationToken cancellationToken) {
+        return new(true , [] , await _unitOfWork.Queries.ChatRequests.GetSendRequestsAsync(request.MyId));
+    }
+}
