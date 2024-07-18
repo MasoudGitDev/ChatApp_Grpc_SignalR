c6407990bed3d4be3d936024142514673d3a2f9a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-18 15:53:25 +0330

    Refactor : composit the model and handler and change name

--- Services/Chats/Apps.Chats/Shared/Models/FindMeByUserNameRequest.cs
+++
@@ -1,5 +0,0 @@
-﻿using Domains.Auth.User.Aggregate;
-using MediatR;
-
-namespace Apps.Chats.Shared.Models;
-public record FindMeByUserNameRequest(string UserName) : IRequest<AppUser>;
---
+++ Services/Chats/Apps.Chats/Shared/Queries/FindMeByUserName.cs
@@ -0,0 +1,17 @@
+﻿using Domains.Auth.User.Aggregate;
+using MediatR;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Chats.Shared.Queries;
+
+public sealed record FindMeByUserName(string UserName) : IRequest<AppUser> {
+    public static FindMeByUserName New(string userName) => new(userName);
+}
+
+//=================== Handler
+internal sealed class FindMeByUserNameHandler(IChatUOW _unitOfWork) : IRequestHandler<FindMeByUserName , AppUser> {
+    public async Task<AppUser> Handle(FindMeByUserName request , CancellationToken cancellationToken) {
+        return await _unitOfWork.Queries.Users.FindByUserNameAsync(request.UserName) ?? AppUser.Empty;
+    }
+}
+
