743d9123314c52a76f74cd0a93abb703432f1985
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-20 11:23:47 +0330

    refactor and update : use dispose for hubconnection in disposable and write other codes better

--- Presentations/Client.ChatApp/Pages/Home.razor.cs
+++ Presentations/Client.ChatApp/Pages/Home.razor.cs
@@ -10,24 +10,34 @@ namespace Client.ChatApp.Pages;
 
 public class HomeViewHandler : ComponentBase , IAsyncDisposable {
 
+    // =============================== init by DI
     [Inject]
     private NavigationManager NavManager { get; set; } = null!;
 
     [Inject]
     private GrpcChannel GrpcChannel { get; set; } = null!;
 
-    private UserQeriesRPCs.UserQeriesRPCsClient Queries;
+    //==================================== private fields and props
+    private UserQeriesRPCs.UserQeriesRPCsClient Queries => new(GrpcChannel);
 
-    private HubConnection _hubConnection;
+    private HubConnection _hubConnection = null!;
 
+    //======================================= Visible fields or props in razor
     protected LinkedList<UserBasicInfoDto> Users = new();
     protected void GoUserProfile(string profileId) => NavManager.NavigateTo("/Profile/" +  profileId);
 
+    //============================
     protected override async Task OnInitializedAsync() {
         Users.Clear();
-        Queries = new(GrpcChannel);
         Users = await GetUsers();
-        _hubConnection = new HubConnectionBuilder().WithUrl(NavManager.ToAbsoluteUri("https://localhost:7001/SignUpHub")).Build();        
+        await SignUpHubConfigAsync();
+    }
+
+
+    //==================================== private methods
+    private async Task SignUpHubConfigAsync() {
+        var absoluteUri = "https://localhost:7001/SignUpHub";
+        _hubConnection = new HubConnectionBuilder().WithUrl(NavManager.ToAbsoluteUri(absoluteUri)).Build();
         _hubConnection.On<UserBasicInfoDto>("GetNewUser" , async (user) => {
             Users.AddFirst(user);
             await InvokeAsync(StateHasChanged);
@@ -35,12 +45,15 @@ public class HomeViewHandler : ComponentBase , IAsyncDisposable {
         await _hubConnection.StartAsync();
     }
 
-
     private async Task<LinkedList<UserBasicInfoDto>> GetUsers()
         => await (Queries.GetUsers(new Protos.Empty()))
         .ToLinkedListAsync<UserBasicInfoMsg,UserBasicInfoDto>();
 
+
+    //============================== Disposable
     public async ValueTask DisposeAsync() {
+       await  _hubConnection.StopAsync();
+       await _hubConnection.DisposeAsync();
        await GrpcChannel.ShutdownAsync();
     }
 }
