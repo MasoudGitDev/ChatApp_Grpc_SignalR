57b8081ebe215fa1773c081b3ffa599d305d7c56
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-20 12:58:09 +0330

    create : OnlineUser aggregate

---
+++ Services/Auth/Domains.Auth/Online/Aggregate/OnlineUser.cs
@@ -0,0 +1,10 @@
+﻿using Domains.Auth.User.Aggregate;
+
+namespace Domains.Auth.Online.Aggregate;
+public partial class OnlineUser {
+    public Guid UserId { get; private set; }
+    public DateTime OnlineAt { get; set; } = DateTime.UtcNow;
+
+    //================ relations
+    public AppUser User { get; private set; } = null!;
+}
---
+++ Services/Auth/Domains.Auth/Online/Aggregate/OnlineUserCommands.cs
@@ -0,0 +1,12 @@
+﻿namespace Domains.Auth.Online.Aggregate;
+public partial class OnlineUser {
+    public static OnlineUser Create(Guid userId)
+        => new() {
+            UserId = userId
+        };
+
+    public Task UpdateAsync(DateTime onlineAt) {
+        OnlineAt = onlineAt;
+        return Task.CompletedTask;
+    }
+}
