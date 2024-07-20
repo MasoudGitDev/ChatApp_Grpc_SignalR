d4e29ffb17b49656fda4856bdb4aa0ed509b50d8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-20 11:02:00 +0330

    create : create ToListAsync and ToLinkedListAsync  methods to handler grpc stream response

---
+++ Presentations/Client.ChatApp/Extensions/GrpcServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using Mapster;
+
+namespace Client.ChatApp.Extensions;
+
+public static class GrpcServiceExtensions {
+    public static async Task<List<TModel>> ToListAsync<TMessage, TModel>(this AsyncServerStreamingCall<TMessage> streamingCall) {
+        var list = new List<TModel>();
+        while(await streamingCall.ResponseStream.MoveNext(new CancellationToken())) {
+            list.Add(streamingCall.ResponseStream.Current.Adapt<TModel>());
+        }
+        return list;
+    }
+    public static async Task<LinkedList<TModel>> ToLinkedListAsync<TMessage, TModel>(this AsyncServerStreamingCall<TMessage> streamingCall) {
+        var list = new LinkedList<TModel>();
+        while(await streamingCall.ResponseStream.MoveNext(new CancellationToken())) {
+            list.AddLast(streamingCall.ResponseStream.Current.Adapt<TModel>());
+        }
+        return list;
+    }
+}
