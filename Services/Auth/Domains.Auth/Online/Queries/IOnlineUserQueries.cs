9e034ec6f9732021b5f9a8f8f1d749fbad5a6e28
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-21 10:07:10 +0330

    create : basic queries to get onlineUser(s)

---
+++ Services/Auth/Domains.Auth/Online/Queries/IOnlineUserQueries.cs
@@ -0,0 +1,7 @@
+﻿using Domains.Auth.Online.Aggregate;
+
+namespace Domains.Auth.Online.Queries;
+public interface IOnlineUserQueries {
+    Task<OnlineUser> GetByIdAsync(Guid userId);
+    Task<List<OnlineUser>> GetAllAsync();
+}
