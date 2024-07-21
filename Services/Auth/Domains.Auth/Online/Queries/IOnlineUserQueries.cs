ea49a924e71e7a68e318586dc92b052c24cd9ef3
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-21 10:19:07 +0330

    update : make nullable when OnlineUser is null

--- Services/Auth/Domains.Auth/Online/Queries/IOnlineUserQueries.cs
+++ Services/Auth/Domains.Auth/Online/Queries/IOnlineUserQueries.cs
@@ -2,6 +2,6 @@
 
 namespace Domains.Auth.Online.Queries;
 public interface IOnlineUserQueries {
-    Task<OnlineUser> GetByIdAsync(Guid userId);
+    Task<OnlineUser?> GetByIdAsync(Guid userId);
     Task<List<OnlineUser>> GetAllAsync();
 }
