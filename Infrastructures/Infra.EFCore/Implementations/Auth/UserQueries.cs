5bae8444c765a4ee4380491a0b8e0ac53798cb6b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-21 10:15:19 +0330

    refactor : change possiotion

--- Infrastructures/Infra.EFCore/Implementations/Auth/UserQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Auth/UserQueries.cs
@@ -4,22 +4,27 @@ using Domains.Auth.User.ValueObjects;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infra.EFCore.Implementations.Accounts;
+namespace Infra.EFCore.Implementations.Auth;
 
-internal class UserQueries(UserManager<AppUser> _userManager) : IUserQueries {
-    public async Task<AppUser?> FindByIdAsync(Guid userId) {
+internal class UserQueries(UserManager<AppUser> _userManager) : IUserQueries
+{
+    public async Task<AppUser?> FindByIdAsync(Guid userId)
+    {
         return await _userManager.FindByIdAsync(userId.ToString());
     }
 
-    public async Task<AppUser?> FindByProfileIdAsync(ProfileId profileId) {
+    public async Task<AppUser?> FindByProfileIdAsync(ProfileId profileId)
+    {
         return await _userManager.Users.Where(x => x.ProfileId == profileId).FirstOrDefaultAsync();
     }
 
-    public async Task<AppUser?> FindByUserNameAsync(string username) {
+    public async Task<AppUser?> FindByUserNameAsync(string username)
+    {
         return await _userManager.FindByNameAsync(username);
     }
 
-    public async Task<List<AppUser>> GetUsersAsync() {
+    public async Task<List<AppUser>> GetUsersAsync()
+    {
         return await _userManager.Users.ToListAsync();
     }
 }
