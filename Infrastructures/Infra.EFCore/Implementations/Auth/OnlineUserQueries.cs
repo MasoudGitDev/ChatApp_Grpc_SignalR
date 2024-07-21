d2706aea0dd89098b9c0d6f366f2e2bfef099fa8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-21 10:15:50 +0330

    create : implement the IOnlineUserQueries

---
+++ Infrastructures/Infra.EFCore/Implementations/Auth/OnlineUserQueries.cs
@@ -0,0 +1,15 @@
+﻿using Domains.Auth.Online.Aggregate;
+using Domains.Auth.Online.Queries;
+using Infra.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.EFCore.Implementations.Auth;
+internal class OnlineUserQueries(AppDbContext _dbContext) : IOnlineUserQueries {
+    public async Task<List<OnlineUser>> GetAllAsync() {
+        return await _dbContext.OnlineUsers.ToListAsync();
+    }
+
+    public async Task<OnlineUser?> GetByIdAsync(Guid userId) {
+        return await _dbContext.OnlineUsers.Where(x=>x.UserId == userId).FirstOrDefaultAsync();
+    }
+}
