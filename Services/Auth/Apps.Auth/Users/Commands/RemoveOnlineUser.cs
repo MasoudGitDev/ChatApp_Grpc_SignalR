7d35ff774d057bb500cb23cf86750e23f11e04e8
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:38:25 +0330

    create : to handle remove user's online status

---
+++ Services/Auth/Apps.Auth/Users/Commands/RemoveOnlineUser.cs
@@ -0,0 +1,25 @@
+﻿using Domains.Auth.Online.Aggregate;
+using MediatR;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Auth.Users.Commands;
+public sealed record RemoveOnlineUser(Guid MyId) : IRequest<ResultStatus> {
+    public static RemoveOnlineUser New(Guid myId) => new(myId);
+}
+
+
+
+//======================= Handler
+internal sealed class RemoveOnlineUserHandler(IChatUOW _unitOfWork) : IRequestHandler<RemoveOnlineUser , ResultStatus> {
+    public async Task<ResultStatus> Handle(RemoveOnlineUser request , CancellationToken cancellationToken) {
+        string message = "You has not been in OnlineUsers!";
+        var onlineUser = await _unitOfWork.Queries.OnlineUsers.GetByIdAsync(request.MyId);
+        if(onlineUser is not null) {
+            message = "You has been removed from OnlineUsers successfully.";
+            await _unitOfWork.DeleteAsync(onlineUser);
+            await _unitOfWork.SaveChangeAsync();
+        }       
+        return SuccessResults.Ok(message);
+    }
+}
