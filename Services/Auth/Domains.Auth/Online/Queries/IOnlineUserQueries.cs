7b34238ce529a2186626611499f1ac306670cdec
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:32:57 +0330

    update : add GetIDs and GetInfoById

--- Services/Auth/Domains.Auth/Online/Queries/IOnlineUserQueries.cs
+++ Services/Auth/Domains.Auth/Online/Queries/IOnlineUserQueries.cs
@@ -1,7 +1,14 @@
 ﻿using Domains.Auth.Online.Aggregate;
+using Domains.Auth.User.Aggregate;
 
 namespace Domains.Auth.Online.Queries;
 public interface IOnlineUserQueries {
     Task<OnlineUser?> GetByIdAsync(Guid userId);
     Task<List<OnlineUser>> GetAllAsync();
+    Task<List<Guid>> GetIdsAsync();
+
+    /// <summary>
+    /// This Methods can be a GetUserWithOnlineStatus() method.
+    /// </summary>
+    Task<AppUser?> GetInfoByIdAsync(Guid userId);
 }
