da244513c04b89f0c3aec474e515ba6738dc4212
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:21:19 +0330

    update : add New() static method

--- Shared.Server/Dtos/User/UserBasicInfoDto.cs
+++ Shared.Server/Dtos/User/UserBasicInfoDto.cs
@@ -1,2 +1,5 @@
 ﻿namespace Shared.Server.Dtos.User;
-public record UserBasicInfoDto(string ProfileId , string DisplayName , string ImageUrl = "");
+public record UserBasicInfoDto(string ProfileId , string DisplayName , string ImageUrl = "") {
+    public static UserBasicInfoDto New(string profileId , string displayName , string imageUrl = "")
+        => new(profileId , displayName , imageUrl);
+}
