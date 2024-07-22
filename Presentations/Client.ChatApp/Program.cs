00d59553aee41f8ac9da6c01470999042efdfeeb
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:49:01 +0330

    refacot and update : add UserCommandsRPCs and refactor other services

--- Presentations/Client.ChatApp/Program.cs
+++ Presentations/Client.ChatApp/Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using Client.ChatApp;
 using Client.ChatApp.Protos;
+using Client.ChatApp.Protos.Users;
 using Client.ChatApp.Services;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
@@ -26,32 +27,14 @@ using var gRPCChannel = GrpcChannel.ForAddress("https://localhost:7001" , new Gr
 });
 
 builder.Services.AddScoped(x => gRPCChannel);
+builder.Services.AddScoped(services => new ChatMessageRPCs.ChatMessageRPCsClient(gRPCChannel));
+builder.Services.AddScoped(services => new ChatRequestCommandsRPCs.ChatRequestCommandsRPCsClient(gRPCChannel));
+builder.Services.AddScoped(services =>new ChatRequestQueryRPCs.ChatRequestQueryRPCsClient(gRPCChannel));
+builder.Services.AddScoped(service => new AccountRPCs.AccountRPCsClient(gRPCChannel));
+builder.Services.AddScoped(service => new ContactRPCs.ContactRPCsClient(gRPCChannel));
+builder.Services.AddScoped(service => new SharedRpcs.SharedRpcsClient(gRPCChannel));
+builder.Services.AddScoped(service => new UserCommandsRPCs.UserCommandsRPCsClient(gRPCChannel));
 
-builder.Services.AddScoped<ChatMessageRPCs.ChatMessageRPCsClient>(services => {
-    return new ChatMessageRPCs.ChatMessageRPCsClient(gRPCChannel);
-});
-
-builder.Services.AddScoped<ChatRequestCommandsRPCs.ChatRequestCommandsRPCsClient>(services => {
-    return new ChatRequestCommandsRPCs.ChatRequestCommandsRPCsClient(gRPCChannel);
-});
-
-builder.Services.AddScoped<ChatRequestQueryRPCs.ChatRequestQueryRPCsClient>(services => {
-    return new ChatRequestQueryRPCs.ChatRequestQueryRPCsClient(gRPCChannel);
-});
-
-
-
-builder.Services.AddScoped<AccountRPCs.AccountRPCsClient>(service => {
-    return new AccountRPCs.AccountRPCsClient(gRPCChannel);
-});
-
-builder.Services.AddScoped<ContactRPCs.ContactRPCsClient>(service => {
-    return new ContactRPCs.ContactRPCsClient(gRPCChannel);
-});
-
-builder.Services.AddScoped(service => {
-    return new SharedRpcs.SharedRpcsClient(gRPCChannel);
-});
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<AuthenticationStateProvider , AuthStateProvider>();
