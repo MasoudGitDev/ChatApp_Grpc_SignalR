99ef9800ebed587f17055fb67eeab2b72663693c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:42:01 +0330

    refactor : change input type of SendNewUser form UserBasicInfoDto to OnlineUserDto

--- Presentations/Server.ChatApp/Hubs/Accounts/SignUpHub.cs
+++ Presentations/Server.ChatApp/Hubs/Accounts/SignUpHub.cs
@@ -4,7 +4,7 @@ using Shared.Server.Dtos.User;
 namespace Server.ChatApp.Hubs.Accounts;
 
 public class SignUpHub :Hub{
-    public async Task SendNewUser(UserBasicInfoDto user) {
+    public async Task SendNewUser(OnlineUserDto user) {
         await Clients.All.SendAsync("GetNewUser" , user);
     }
 }
