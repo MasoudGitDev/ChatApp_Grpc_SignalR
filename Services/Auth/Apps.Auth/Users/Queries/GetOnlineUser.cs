2f18f1b97241a280d5a3ed44ac7914863a27d28a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:34:59 +0330

    update : add factory method and change result message

--- Services/Auth/Apps.Auth/Users/Queries/GetOnlineUser.cs
+++ Services/Auth/Apps.Auth/Users/Queries/GetOnlineUser.cs
@@ -4,15 +4,16 @@ using MediatR;
 using Shared.Server.Models.Results;
 
 namespace Apps.Auth.Users.Queries;
-public sealed record GetOnlineUser(Guid UserId) : IRequest<ResultStatus<OnlineUser>>;
+public sealed record GetOnlineUser(Guid UserId) : IRequest<ResultStatus<OnlineUser>> {
+    public static GetOnlineUser New(Guid userId) => new(userId);
+}
 
 internal sealed class GetOnlineUserHandler(IOnlineUserQueries _queries) :
     IRequestHandler<GetOnlineUser , ResultStatus<OnlineUser>> {
     public async Task<ResultStatus<OnlineUser>> Handle(GetOnlineUser request , CancellationToken cancellationToken) {
         var findOnlineUser = await _queries.GetByIdAsync(request.UserId);
         if(findOnlineUser is null) {
-            return ErrorResults.NotFound($"There is no any record with id:{request.UserId} in OnlineUsers table." ,
-                findOnlineUser);
+            return ErrorResults.NotFound($"Not Found any online user with id:{request.UserId}" , findOnlineUser);
         }
         return SuccessResults.Ok(findOnlineUser);
     }
