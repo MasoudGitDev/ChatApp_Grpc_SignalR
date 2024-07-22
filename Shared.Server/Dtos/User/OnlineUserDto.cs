6f0e3ede20d6c12f411a16f8a185ac32ee53c27d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:28:15 +0330

    create : to handle OnlineUsers info

---
+++ Shared.Server/Dtos/User/OnlineUserDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Server.Dtos.User;
+
+public record OnlineUserDto(string ProfileId , string DisplayName , string ImageUrl = "" , bool IsOnline = false) {
+    public static OnlineUserDto New(string profileId , string displayName , string imageUrl = "" , bool isOnline = false)
+        => new( profileId,displayName,imageUrl, isOnline);
+}
