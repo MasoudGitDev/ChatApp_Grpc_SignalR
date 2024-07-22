1ff26476086aa453093fa340fd74319e3bb0312a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:38:12 +0330

    create : to handle Create Or Update user's online status

---
+++ Services/Auth/Apps.Auth/Users/Commands/CreateOrUpdateOnlineUser.cs
@@ -0,0 +1,25 @@
+﻿using Domains.Auth.Online.Aggregate;
+using MediatR;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Auth.Users.Commands;
+public sealed record CreateOrUpdateOnlineUser(Guid MyId) : IRequest<ResultStatus> {
+    public static CreateOrUpdateOnlineUser New(Guid myId) => new(myId);
+}
+
+internal sealed class CreateOrUpdateOnlineUserHandler(IChatUOW _unitOfWork) : IRequestHandler<CreateOrUpdateOnlineUser , ResultStatus> {
+    public async Task<ResultStatus> Handle(CreateOrUpdateOnlineUser request , CancellationToken cancellationToken) {
+        string message = "You has been added to OnlineUsers successfully.";
+        var onlineUser = await _unitOfWork.Queries.OnlineUsers.GetByIdAsync(request.MyId);
+        if(onlineUser is null) {
+            await _unitOfWork.CreateAsync(OnlineUser.Create(request.MyId));
+        }
+        else {
+            message = "Your Online Status has been changed successfully";
+            await onlineUser.UpdateAsync(DateTime.UtcNow);
+        }
+        await _unitOfWork.SaveChangeAsync();
+        return SuccessResults.Ok(message);
+    }
+}
