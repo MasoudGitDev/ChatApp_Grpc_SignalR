ccc1c7db26d9ca2be5588ed5d001e3e04751cfbf
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-22 11:40:22 +0330

    update : implement GetOnlineUsers , GetUsersWithOnlineStatus

--- Presentations/Server.ChatApp/ServiceHandlers/Users/QueriesHandler.cs
+++ Presentations/Server.ChatApp/ServiceHandlers/Users/QueriesHandler.cs
@@ -1,29 +1,50 @@
-﻿using Apps.Auth.Users.Queries;
-using Grpc.Core;
+﻿using Grpc.Core;
 using Mapster;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Extensions;
 using Server.ChatApp.Protos;
 using Server.ChatApp.Protos.Users;
 using Shared.Server.Dtos.User;
+using Shared.Server.Extensions;
 using Shared.Server.Models.Results;
+using Queries = Apps.Auth.Users.Queries;
 
 namespace Server.ChatApp.ServiceHandlers.Users;
 
 [Authorize]
 public class QueriesHandler(IMediator _mediator) : UserQeriesRPCs.UserQeriesRPCsBase {
-
-
-    /// <summary>
-    ///  usage in home page
-    /// </summary>
-    public override async Task GetUsers(Empty request , IServerStreamWriter<UserBasicInfoMsg> responseStream , ServerCallContext context) {
-        ResultStatus<List<UserBasicInfoDto>> result = await _mediator.Send(GetUsersBasicInfo.New());
+    public override async Task GetOnlineUsers(Empty request , IServerStreamWriter<OnlineUserMsg> responseStream , ServerCallContext context) {
+        ResultStatus<List<UserBasicInfoDto>> result = await _mediator.Send(Queries.GetOnlineUsers.New());
         if(!result.IsSuccessful || result.Model is null) {
             throw new RpcException(Status.DefaultCancelled);
         }
-        foreach(var user in result.Model) { 
+        foreach(var user in result.Model) {
+            await responseStream.WriteAsync(user.Adapt<OnlineUserMsg>());
+        }
+    }
+
+    public override async Task GetUsers(Empty request , IServerStreamWriter<UserBasicInfoMsg> responseStream , ServerCallContext context) {
+        ResultStatus<List<UserBasicInfoDto>> result = await _mediator.Send(Queries.GetUsersBasicInfo.New());
+        if(!result.IsSuccessful || result.Model is null) {
+            throw new RpcException(Status.DefaultCancelled);
+        }
+        foreach(var user in result.Model) {
             await responseStream.WriteAsync(user.Adapt<UserBasicInfoMsg>());
-        }        
+        }
+    }
+
+    public override async Task GetUsersWithOnlineStatus(Empty request , IServerStreamWriter<OnlineUserMsg> responseStream , ServerCallContext context) {
+        var result = (await _mediator.Send(Queries.GetUsersWithOnlineStatus.New()));
+        if(!result.IsSuccessful || result.Model is null) {
+            throw new RpcException(Status.DefaultCancelled);
+        }
+        foreach(var user in result.Model) {
+            await responseStream.WriteAsync(user.Adapt<OnlineUserMsg>());
+        }
+    }
+
+    public override async Task<ResultMsg> IsOnline(PersonMsg request , ServerCallContext context) {
+        return ( await _mediator.Send(Queries.GetOnlineUser.New(request.Id.AsGuid())) ).AsCommonResult();
     }
 }
