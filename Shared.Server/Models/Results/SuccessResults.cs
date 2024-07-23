c82e85e1655e13fe2dc6a4a504cebfd092e04438
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-23 15:40:03 +0330

    update : add new version of Ok<T> ( with message param)

--- Shared.Server/Models/Results/SuccessResults.cs
+++ Shared.Server/Models/Results/SuccessResults.cs
@@ -9,9 +9,10 @@ public record class SuccessResults(string Code , string Message) {
         => new(true , [MessageDescription.Create(nameof(Created) , "The new record is created successfully.")]);
 
     public static ResultStatus Ok(string message)
-        => new(true , [MessageDescription.Create(nameof(Ok) , message)]);
+        => new(true , [MessageDescription.Create("Ok" , message)]);
 
     // for queries
-    public static ResultStatus<T> Ok<T>(T data)
-        => new(true , [],data);
+    public static ResultStatus<T> Ok<T>(T data) => new(true , [],data);
+    public static ResultStatus<T> Ok<T>(string message , T data)
+    => new(true , [MessageDescription.Create( "Ok" , message)] , data);
 }
