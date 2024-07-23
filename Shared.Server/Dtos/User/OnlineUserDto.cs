5e770a971f2ff3aca097cb8e6bea9a35d5793b06
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-23 15:41:42 +0330

    update : use UserBasicInfo instead of using some related params

--- Shared.Server/Dtos/User/OnlineUserDto.cs
+++ Shared.Server/Dtos/User/OnlineUserDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Shared.Server.Dtos.User;
 
-public record OnlineUserDto(string ProfileId , string DisplayName , string ImageUrl = "" , bool IsOnline = false) {
-    public static OnlineUserDto New(string profileId , string displayName , string imageUrl = "" , bool isOnline = false)
-        => new( profileId,displayName,imageUrl, isOnline);
+public record OnlineUserDto(UserBasicInfoDto BasicInfo , bool IsOnline , bool IsInContacts = false) {
+    public static OnlineUserDto New(UserBasicInfoDto basicInfo , bool isOnline , bool isInContacts = false)
+        => new(basicInfo, isOnline ,isInContacts);
 }
