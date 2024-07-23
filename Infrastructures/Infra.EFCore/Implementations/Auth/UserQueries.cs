eb590a1e619165cbd0fa0b4157e686caeae47275
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-23 20:13:23 +0330

    update : add and implement FindByEmail for users

--- Infrastructures/Infra.EFCore/Implementations/Auth/UserQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Auth/UserQueries.cs
@@ -3,32 +3,31 @@ using Domains.Auth.User.Aggregate;
 using Domains.Auth.User.ValueObjects;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Shared.Server.DataStructures;
 
 namespace Infra.EFCore.Implementations.Auth;
 
-internal class UserQueries(UserManager<AppUser> _userManager) : IUserQueries
-{
-    public async Task<AppUser?> FindByIdAsync(Guid userId)
-    {
+internal class UserQueries(UserManager<AppUser> _userManager) : IUserQueries {
+    public async Task<AppUser?> FindByEmailAsync(string email) {
+        return await _userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<AppUser?> FindByIdAsync(Guid userId) {
         return await _userManager.FindByIdAsync(userId.ToString());
     }
 
-    public async Task<AppUser?> FindByProfileIdAsync(ProfileId profileId)
-    {
+    public async Task<AppUser?> FindByProfileIdAsync(ProfileId profileId) {
         return await _userManager.Users.Where(x => x.ProfileId == profileId).FirstOrDefaultAsync();
     }
 
-    public async Task<AppUser?> FindByUserNameAsync(string username)
-    {
+    public async Task<AppUser?> FindByUserNameAsync(string username) {
         return await _userManager.FindByNameAsync(username);
     }
 
     public async Task<List<AppUser>> GetUsersAsync(int pageNumber = 1 , int size = 20) {
-        
+
         return await _userManager.Users
             .OrderBy(x => x.CreatedAt)
-            .Skip(( pageNumber - 1) * size)
+            .Skip(( pageNumber - 1 ) * size)
             .Take(size)
             .ToListAsync();
     }
--- Services/Auth/Domains.Auth/User/Queries/IUserQueries.cs
+++ Services/Auth/Domains.Auth/User/Queries/IUserQueries.cs
@@ -6,6 +6,7 @@ public interface IUserQueries {
     Task<AppUser?> FindByUserNameAsync(string username);
     Task<AppUser?> FindByIdAsync(Guid userId);
     Task<AppUser?> FindByProfileIdAsync(ProfileId profileId);
+    Task<AppUser?> FindByEmailAsync(string email);
     Task<List<AppUser>> GetUsersAsync(int pageNumber = 1 , int size = 20);
 }
 
