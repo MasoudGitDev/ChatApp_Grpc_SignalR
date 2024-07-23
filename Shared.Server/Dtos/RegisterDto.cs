064d81f28bda31b9a51bb61cb765f77d5901d2a5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-23 20:14:00 +0330

    update : add Id as userId

--- Shared.Server/Dtos/RegisterDto.cs
+++ Shared.Server/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shared.Server.Dtos;
 public class RegisterDto {
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Email { get; set; } = String.Empty;
     public string UserName { get; set; } = String.Empty;
     public string Password { get; set; } = String.Empty;
