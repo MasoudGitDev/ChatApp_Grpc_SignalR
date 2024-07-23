c3ccfb7ca6809fc2c6bf724dc98ed437fd26bde3
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-23 10:28:58 +0330

    create : OnlineStatusHub to handle User Online status

---
+++ Presentations/Server.ChatApp/Hubs/OnlineStatusHub.cs
@@ -0,0 +1,18 @@
+﻿using Apps.Auth.Users.Commands;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Server.ChatApp.Hubs;
+
+
+public class OnlineStatusHub : Hub {
+
+    public async Task SetOnlineStatus(bool isActive) {
+        await Clients.All.SendAsync("GetOnlineStatus" , isActive);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception) {
+
+    }
+}
