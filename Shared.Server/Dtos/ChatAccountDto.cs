11e10ed3382e77d38f7ddde8bf42624885fa358e
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-24 12:27:49 +0330

    update : userId prop

--- Shared.Server/Dtos/ChatAccountDto.cs
+++ Shared.Server/Dtos/ChatAccountDto.cs
@@ -1,5 +1,6 @@
 ﻿namespace Shared.Server.Dtos;
 public class ChatAccountDto {
+    public Guid UserId { get; set; }
     public string LogoUrl { get; set; } = String.Empty;
     public string FullName { get; set; } = String.Empty;
     public int UnReadMessages { get; set; } = 0;
