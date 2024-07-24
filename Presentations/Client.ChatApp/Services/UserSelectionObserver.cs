9666eda2e69a7a5e18f6333a8a49881a71ebfa61
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-24 12:29:14 +0330

    create : to handle SelectedUser from home and use its item in chats

---
+++ Presentations/Client.ChatApp/Services/UserSelectionObserver.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Server.Dtos.User;
+
+namespace Client.ChatApp.Services;  
+internal class UserSelectionObserver {
+
+    public event Action? OnChangeSelection;
+    public UserBasicInfoDto? Item { get; private set; }
+    public void OnSelectedItem(UserBasicInfoDto item) {
+        Item = item;
+        NotifyOnChangeItem();
+    }
+
+    private void NotifyOnChangeItem() => OnChangeSelection?.Invoke();
+}
