dc7d18fce795638e651dd3892b0e628514c0ae56
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-24 12:29:43 +0330

    update : register UserSelectionObserver

--- Presentations/Client.ChatApp/Program.cs
+++ Presentations/Client.ChatApp/Program.cs
@@ -39,6 +39,6 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<AuthenticationStateProvider , AuthStateProvider>();
 
-
+builder.Services.AddSingleton(_ => new UserSelectionObserver());
 
 await builder.Build().RunAsync();
