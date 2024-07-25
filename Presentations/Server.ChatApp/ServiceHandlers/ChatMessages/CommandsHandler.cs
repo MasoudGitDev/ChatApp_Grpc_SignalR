653c1ce7a27beae7532c2c55281bafc8b9939a22
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-25 13:07:29 +0330

    create : create chat_message_commands proto and its handler

---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/CommandsHandler.cs
@@ -0,0 +1,15 @@
+﻿using Apps.Chats.ChatMessages.Commands;
+using Grpc.Core;
+using Mapster;
+using MediatR;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+using Server.ChatApp.Protos.ChatMessages;
+
+namespace Server.ChatApp.ServiceHandlers.ChatMessages;
+
+internal class CommandsHandler(IMediator _mediator) : ChatMessageCommandRPCs.ChatMessageCommandRPCsBase {
+    public override async Task<ResultMsg> Send(ChatMessageMsg request , ServerCallContext context) {
+        return ( await _mediator.Send(request.Adapt<Create>()) ).AsCommonResult();
+    }
+}
