acec6652055ee35d1216293e3b93333b82525ea9
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 14:02:07 +0330

    refactor : change names

---
+++ Shared.Server/Dtos/Chat/ChatItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Server.Dtos.Chat;
+public class ChatItemDto
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid ReceiverId { get; set; }
+    public string DisplayName { get; set; } = string.Empty;
+    public string LogoUrl { get; set; } = string.Empty;
+    public int UnReadMessages { get; set; } = 0;
+}
