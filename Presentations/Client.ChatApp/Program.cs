8f58ff497d2951518d77314dc130331e475110f7
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:28:19 +0330

    save : cs proj save

--- Presentations/Client.ChatApp/Program.cs
+++ Presentations/Client.ChatApp/Program.cs
@@ -28,12 +28,17 @@ using var gRPCChannel = GrpcChannel.ForAddress("https://localhost:7001" , new Gr
 });
 
 builder.Services.AddScoped(x => gRPCChannel);
+
 builder.Services.AddScoped(services => new ChatMessageCommandRPCs.ChatMessageCommandRPCsClient(gRPCChannel));
+builder.Services.AddScoped(services => new ChatMessageQueryRPCs.ChatMessageQueryRPCsClient(gRPCChannel));
+
 builder.Services.AddScoped(services => new ChatRequestCommandsRPCs.ChatRequestCommandsRPCsClient(gRPCChannel));
 builder.Services.AddScoped(services =>new ChatRequestQueryRPCs.ChatRequestQueryRPCsClient(gRPCChannel));
+
 builder.Services.AddScoped(service => new AccountRPCs.AccountRPCsClient(gRPCChannel));
 builder.Services.AddScoped(service => new ContactRPCs.ContactRPCsClient(gRPCChannel));
 builder.Services.AddScoped(service => new SharedRpcs.SharedRpcsClient(gRPCChannel));
+
 builder.Services.AddScoped(service => new OnlineUserCommandsRPCs.OnlineUserCommandsRPCsClient(gRPCChannel));
 
 builder.Services.AddAuthorizationCore();
