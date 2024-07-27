4ab57893fa3d8a1f08e6b7661195898f154e953d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:14:25 +0330

    update : Add GetAll method to handle get all messages

--- Infrastructures/Infra.EFCore/Implementations/Chats/MessageQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Chats/MessageQueries.cs
@@ -1,10 +1,15 @@
 ﻿using Domains.Chats.Message.Aggregate;
 using Domains.Chats.Message.Queries;
 using Infra.EFCore.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infra.EFCore.Implementations.Chats;
 internal class MessageQueries(AppDbContext _dbContext) : IChatMessageQueries {
     public async Task<ChatMessage?> FindByIdAsync(Guid messageId) {
         return await _dbContext.ChatMessages.FindAsync(messageId);
+    }
+
+    public async Task<List<ChatMessage>> GetAllAsync(Guid chatItemId) {
+        return await _dbContext.ChatMessages.Where(x => x.ChatItemId == chatItemId).ToListAsync();
     }
 }
--- Services/Chats/Domains.Chats/Message/Queries/IChatMessageQueries.cs
+++ Services/Chats/Domains.Chats/Message/Queries/IChatMessageQueries.cs
@@ -3,5 +3,6 @@
 namespace Domains.Chats.Message.Queries;
 public interface IChatMessageQueries {
     Task<ChatMessage?> FindByIdAsync(Guid messageId);
+    Task<List<ChatMessage>> GetAllAsync(Guid chatItemId);
 
 }
