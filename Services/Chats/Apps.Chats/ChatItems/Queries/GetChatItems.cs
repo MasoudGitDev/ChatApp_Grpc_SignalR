7ae32a439e50206b3faf15fc2622d63ad1ffe11a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:18:24 +0330

    create : GetChatItems and its mediatR Handler

---
+++ Services/Chats/Apps.Chats/ChatItems/Queries/GetChatItems.cs
@@ -0,0 +1,41 @@
+﻿using Domains.Chats.Item.Aggregate;
+using MediatR;
+using Shared.Server.Dtos.Chat;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Chats.ChatItems.Queries;
+public record GetChatItems(Guid MyId , int PageNumber = 1 , int PageSize = 20) : IRequest<ResultStatus<List<ChatItemDto>>> {
+    public static GetChatItems New(Guid myId , int pageNumber = 1 , int pageSize = 20) => new(myId , pageNumber , pageSize);
+}
+
+//=============== handler
+internal sealed class GetChatItemsHandler(IChatUOW _unitOfWork) : IRequestHandler<GetChatItems , ResultStatus<List<ChatItemDto>>> {
+    public async Task<ResultStatus<List<ChatItemDto>>> Handle(GetChatItems request , CancellationToken cancellationToken) {
+        try {
+            var chatItems = await _unitOfWork.Queries.ChatItems.GetByIdAsync(request.MyId,request.PageNumber,request.PageSize);
+            var itemDTOs = new List<ChatItemDto>();
+            foreach(var chatItem in chatItems) {
+                var findReceiver = await _unitOfWork.Queries.Users.FindByIdAsync(GetReceiverId(request.MyId,chatItem));
+                if(findReceiver is null) {
+                    continue;
+                }
+                itemDTOs.Add(new() {
+                    Id = chatItem.Id ,
+                    DisplayName = findReceiver.DisplayName ,
+                    LogoUrl = findReceiver.ImageUrl ,
+                    ReceiverId = chatItem.ReceiverId ,
+                    UnReadMessages = 0
+                });
+            }
+            return SuccessResults.Ok(itemDTOs);
+        }
+        catch(Exception ex) { 
+            return ErrorResults.Canceled<List<ChatItemDto>>(ex.Message);
+        }
+    }
+
+    private static Guid GetReceiverId(Guid myId , ChatItem item) {
+        return myId == item.RequesterId ? item.ReceiverId : item.RequesterId;
+    }
+}
