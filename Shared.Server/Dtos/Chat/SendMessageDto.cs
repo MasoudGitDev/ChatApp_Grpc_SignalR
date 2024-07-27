b62fd6c5d4c1716f19dfd2ae1ca93db5f468bc8a
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 14:03:56 +0330

    create : to handle send message models

---
+++ Shared.Server/Dtos/Chat/SendMessageDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Server.Dtos.Chat;
+
+public class SendMessageDto {
+    public string Id { get; set; }
+    public string ChatItemId { get; set; }
+    public string SenderId { get; set; }
+    public string ReceiverId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public string FileUrl { get; set; } = string.Empty;
+    public string DisplayName { get; set; }
+}
