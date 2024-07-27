28b9ef6878ab1eeaff3b23e53092da904b6e8f28
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:22:43 +0330

    create : ChatItem grpc Commands and Queries service and them implementations

---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatItems/CommandsHandler.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+using Shared.Server.Extensions;
+using Commands = Apps.Chats.ChatItems.Commands;
+
+namespace Server.ChatApp.ServiceHandlers.ChatItems;
+
+[Authorize]
+internal class CommandsHandler(IMediator _mediator) : ChatItemCommandRPCs.ChatItemCommandRPCsBase {
+    public override async Task<ResultMsg> Create(PersonMsg request , ServerCallContext context)
+        => ( await _mediator.Send(Commands.Create.New(await SharedMethods.GetMyIdAsync(context , _mediator) ,
+            request.Id.AsGuid())) )
+        .AsCommonResult();
+
+    public override async Task<ResultMsg> Remove(RowMsg request , ServerCallContext context)
+         => ( await _mediator.Send(Commands.Remove.New(request.RowId.AsGuid())) )
+        .AsCommonResult();
+}
---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatItems/QueriesHandler.cs
@@ -0,0 +1,16 @@
+﻿using Apps.Chats.ChatItems.Queries;
+using Grpc.Core;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+
+namespace Server.ChatApp.ServiceHandlers.ChatItems;
+
+[Authorize]
+public class QueriesHandler(IMediator _mediator) : ChatItemQueryRPCs.ChatItemQueryRPCsBase {
+    public override async Task<ChatItemResultMsg> GetAll(Empty request , ServerCallContext context) {
+        return ( await _mediator.Send(GetChatItems.New(await SharedMethods.GetMyIdAsync(context , _mediator))) )
+            .AsChatItemResult();
+    }
+}
