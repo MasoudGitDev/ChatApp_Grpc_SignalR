ee2ba19c5e2cc059cf6e1215b63c451f62627f2d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:21:32 +0330

    create : GetMessages and its mediatR handler

---
+++ Services/Chats/Apps.Chats/ChatMessages/Queries/GetMessages.cs
@@ -0,0 +1,24 @@
+﻿using Mapster;
+using MediatR;
+using Shared.Server.Dtos.Chat;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Chats.ChatMessages.Queries;
+public sealed record GetMessages(Guid ChatItemId) : IRequest<ResultStatus<List<GetMessageDto>>> {
+    public static GetMessages New(Guid chatItemId) => new(chatItemId);
+}
+
+
+//=================== handler
+internal sealed class GetMessagesHandler(IChatUOW _unitOfWork) : IRequestHandler<GetMessages , ResultStatus<List<GetMessageDto>>> {
+    public async Task<ResultStatus<List<GetMessageDto>>> Handle(GetMessages request , CancellationToken cancellationToken) {
+        try {
+            var messages = await _unitOfWork.Queries.ChatMessages.GetAllAsync(request.ChatItemId);
+            return SuccessResults.Ok(messages.Adapt<List<GetMessageDto>>());
+        }
+        catch(Exception e) { 
+            return ErrorResults.Canceled<List<GetMessageDto>>(e.Message);
+        }
+    }
+}
