94f874e213ac4443bb66d58d6619443fff34e0c0
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-27 17:20:59 +0330

    create : chat Message Commads and Queries grpc services and them implementations and remove prev ChatMessageCommand

--- Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/CommandsHandler.cs
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/CommandsHandler.cs
@@ -9,7 +9,7 @@ using Server.ChatApp.Protos.ChatMessages;
 namespace Server.ChatApp.ServiceHandlers.ChatMessages;
 
 internal class CommandsHandler(IMediator _mediator) : ChatMessageCommandRPCs.ChatMessageCommandRPCsBase {
-    public override async Task<ResultMsg> Send(ChatMessageMsg request , ServerCallContext context) {
+    public override async Task<ResultMsg> Send(SendMessageMsg request , ServerCallContext context) {
         return ( await _mediator.Send(request.Adapt<Create>()) ).AsCommonResult();
     }
 }
---
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/QueriesHandler.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using MediatR;
+using Server.ChatApp.Extensions;
+using Server.ChatApp.Protos;
+using Server.ChatApp.Protos.ChatMessages;
+using Shared.Server.Extensions;
+using Messages = Apps.Chats.ChatMessages.Queries;
+
+namespace Server.ChatApp.ServiceHandlers.ChatMessages;
+
+public class QueriesHandler(IMediator _mediator) : ChatMessageQueryRPCs.ChatMessageQueryRPCsBase {
+    public override Task<GetChatMessageMsg> GetMessage(TableMsg request , ServerCallContext context) {
+        return base.GetMessage(request , context);
+    }
+
+    public override async Task<ChatMessageResult> GetMessages(TableMsg request , ServerCallContext context) {
+        return ( await _mediator.Send(Messages.GetMessages.New(request.Id.AsGuid())) ).AsChatMessageResult();
+    }
+}
