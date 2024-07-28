a4fe7aafce1abd344c13e59a6e5062ff547233dd
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-28 18:03:25 +0330

    update : remove IsMyMessage prop

--- Shared.Server/Dtos/Chat/GetMessageDto.cs
+++ Shared.Server/Dtos/Chat/GetMessageDto.cs
@@ -4,7 +4,6 @@ public class GetMessageDto
     public Guid Id { get; set; }
     public Guid ChatItemId { get; set; }
     public Guid SenderId { get; set; }
-    public bool IsMyMessage { get; set; } = false;
     public string Content { get; set; } = string.Empty;
     public string FileUrl { get; set; } = string.Empty;
     public bool IsSent { get; set; }
