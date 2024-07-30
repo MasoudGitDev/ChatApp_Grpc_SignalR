bc7cbbe3dc1a9a0f3f0cbca345aae12d4556d116
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-30 10:57:34 +0330

    update : add filter to avoid dispaling cloud item

--- Services/Chats/Apps.Chats/ChatItems/Queries/GetChatItems.cs
+++ Services/Chats/Apps.Chats/ChatItems/Queries/GetChatItems.cs
@@ -1,5 +1,6 @@
 ﻿using Domains.Chats.Item.Aggregate;
 using MediatR;
+using Microsoft.VisualBasic;
 using Shared.Server.Dtos.Chat;
 using Shared.Server.Models.Results;
 using UnitOfWorks.Abstractions;
@@ -16,6 +17,10 @@ internal sealed class GetChatItemsHandler(IChatUOW _unitOfWork) : IRequestHandle
             var chatItems = await _unitOfWork.Queries.ChatItems.GetByIdAsync(request.MyId,request.PageNumber,request.PageSize);
             var itemDTOs = new List<ChatItemDto>();
             foreach(var chatItem in chatItems) {
+                // Avoid Displaying Cloud Item
+                if(chatItem.ReceiverId == request.MyId && chatItem.RequesterId == request.MyId) { 
+                    continue;
+                }
                 var findReceiver = await _unitOfWork.Queries.Users.FindByIdAsync(GetReceiverId(request.MyId,chatItem));
                 if(findReceiver is null) {
                     continue;
