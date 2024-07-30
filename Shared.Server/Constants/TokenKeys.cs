4a1a762ecd98aa4a3a67c37a59e0241447e1087b
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-30 09:50:41 +0330

    create : new modified TokenKeys

---
+++ Shared.Server/Constants/TokenKeys.cs
@@ -0,0 +1,6 @@
+﻿namespace Shared.Server.Constants;
+public static class TokenKeys {
+    public const string UserId = "UserIdentifier";
+    public const string UserName = "UserName";
+    public const string DisplayName = "DisplayName";
+}
