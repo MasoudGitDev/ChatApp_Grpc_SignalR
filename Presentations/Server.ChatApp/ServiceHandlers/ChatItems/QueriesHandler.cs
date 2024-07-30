3e01817333cefede05abf70a0ee44fd991abb9c5
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-30 10:58:53 +0330

    update : add GetCloudItem to grpc handler

--- Presentations/Server.ChatApp/ServiceHandlers/ChatItems/QueriesHandler.cs
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatItems/QueriesHandler.cs
@@ -1,16 +1,21 @@
-﻿using Apps.Chats.ChatItems.Queries;
-using Grpc.Core;
+﻿using Grpc.Core;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Server.ChatApp.Extensions;
 using Server.ChatApp.Protos;
+using Queries = Apps.Chats.ChatItems.Queries;
 
 namespace Server.ChatApp.ServiceHandlers.ChatItems;
 
 [Authorize]
 public class QueriesHandler(IMediator _mediator) : ChatItemQueryRPCs.ChatItemQueryRPCsBase {
     public override async Task<ChatItemResultMsg> GetAll(Empty request , ServerCallContext context) {
-        return ( await _mediator.Send(GetChatItems.New(await SharedMethods.GetMyIdAsync(context , _mediator))) )
+        return ( await _mediator.Send(Queries.GetChatItems.New(await SharedMethods.GetMyIdAsync(context , _mediator))) )
+            .AsChatItemResult();
+    }
+
+    public override async Task<ChatItemResultMsg> GetCloudItem(Empty request , ServerCallContext context) {
+        return ( await _mediator.Send(Queries.GetCloudItem.New(await SharedMethods.GetMyIdAsync(context , _mediator))) )
             .AsChatItemResult();
     }
 }
