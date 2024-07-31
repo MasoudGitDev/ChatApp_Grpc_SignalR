2881b2c7793f2421497c0c81fac33fcd9e958263
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-31 15:39:03 +0330

    update : add a signalR method to send and receive ChatItem

--- Presentations/Server.ChatApp/Hubs/Chats/ChatMessageHub.cs
+++ Presentations/Server.ChatApp/Hubs/Chats/ChatMessageHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Shared.Server.Dtos.Chat;
+using Shared.Server.Dtos.User;
 
 namespace Server.ChatApp.Hubs.Chats;
 
@@ -7,5 +8,9 @@ namespace Server.ChatApp.Hubs.Chats;
 public class ChatMessageHub : Hub {
     public async Task SendMessage(GetMessageDto msg) {
         await Clients.All.SendAsync("ReceiveMessage" , msg , new CancellationToken());
+    }
+
+    public async Task SendChatItem(UserBasicInfoDto senderInfo , UserBasicInfoDto receiverInfo , Guid chatItemId) {
+        await Clients.All.SendAsync("ReceiveChatItem" , senderInfo , receiverInfo , chatItemId , new CancellationToken());
     }
 }
