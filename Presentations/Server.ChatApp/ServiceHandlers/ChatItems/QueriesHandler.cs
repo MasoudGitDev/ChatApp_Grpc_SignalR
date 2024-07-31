b78917124c9f3ce9cd2882b8cc797c3ec7af0a1c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-07-31 15:37:53 +0330

    update : add GetItem rpc and its impl

--- Presentations/Server.ChatApp/ServiceHandlers/ChatItems/QueriesHandler.cs
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatItems/QueriesHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Server.ChatApp.Extensions;
 using Server.ChatApp.Protos;
+using Shared.Server.Extensions;
 using Queries = Apps.Chats.ChatItems.Queries;
 
 namespace Server.ChatApp.ServiceHandlers.ChatItems;
@@ -17,5 +18,12 @@ public class QueriesHandler(IMediator _mediator) : ChatItemQueryRPCs.ChatItemQue
     public override async Task<ChatItemResultMsg> GetCloudItem(Empty request , ServerCallContext context) {
         return ( await _mediator.Send(Queries.GetCloudItem.New(await SharedMethods.GetMyIdAsync(context , _mediator))) )
             .AsChatItemResult();
+    }
+
+    public override async Task<ChatItemResultMsg> GetItem(PersonMsg request , ServerCallContext context) {
+        return ( await _mediator.Send(Queries.FindChatItem.New(
+            await SharedMethods.GetMyIdAsync(context , _mediator) ,
+            request.Id.AsGuid() )
+        )).AsChatItemResult();
     }
 }
