83932bc53e5305485b8ccb1efdd7eafd6717f27f
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-01 09:43:18 +0330

    update : add usePagination boolean param to handle when i need all data

--- Infrastructures/Infra.EFCore/Implementations/Chats/MessageQueries.cs
+++ Infrastructures/Infra.EFCore/Implementations/Chats/MessageQueries.cs
@@ -9,12 +9,17 @@ internal class MessageQueries(AppDbContext _dbContext) : IChatMessageQueries {
         return await _dbContext.ChatMessages.FindAsync(messageId);
     }
 
-    public async Task<List<ChatMessage>> GetAllAsync(Guid chatItemId , int pageNumber = 1 , int pageSize = 50) {
-        return await _dbContext.ChatMessages
+    public async Task<List<ChatMessage>> GetAllAsync(Guid chatItemId , bool usePagination = false , int pageNumber = 1 , int pageSize = 50) {
+        return usePagination ?
+            await _dbContext.ChatMessages
             .Where(x => x.ChatItemId == chatItemId)
             .Skip((pageNumber - 1) * pageSize)
             .Take(pageSize)
             .OrderBy(x=> x.CreatedAt)            
+            .ToListAsync() :
+            await _dbContext.ChatMessages
+            .Where(x => x.ChatItemId == chatItemId)
+            .OrderBy(x => x.CreatedAt)
             .ToListAsync();
     }
 }
