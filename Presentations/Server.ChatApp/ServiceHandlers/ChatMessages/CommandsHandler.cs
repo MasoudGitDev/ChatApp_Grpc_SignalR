da415ed45590a5561bd502ef13726378f2ce5d48
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-03 10:09:28 +0330

    udpate : add markMessagesAsRead

--- Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/CommandsHandler.cs
+++ Presentations/Server.ChatApp/ServiceHandlers/ChatMessages/CommandsHandler.cs
@@ -1,15 +1,20 @@
-﻿using Apps.Chats.ChatMessages.Commands;
-using Grpc.Core;
+﻿using Grpc.Core;
 using Mapster;
 using MediatR;
 using Server.ChatApp.Extensions;
 using Server.ChatApp.Protos;
 using Server.ChatApp.Protos.ChatMessages;
+using Shared.Server.Extensions;
+using Commands = Apps.Chats.ChatMessages.Commands;
 
 namespace Server.ChatApp.ServiceHandlers.ChatMessages;
 
 internal class CommandsHandler(IMediator _mediator) : ChatMessageCommandRPCs.ChatMessageCommandRPCsBase {
+    public override async Task<ResultMsg> MarkMessagesAsRead(TableMsg request , ServerCallContext context) {
+        return ( await _mediator.Send(Commands.MarkMessagesAsRead.New(request.Id.AsGuid())) ).AsCommonResult();
+    }
+
     public override async Task<ResultMsg> Send(SendMessageMsg request , ServerCallContext context) {
-        return ( await _mediator.Send(request.Adapt<Create>()) ).AsCommonResult();
+        return ( await _mediator.Send(request.Adapt<Commands.Create>()) ).AsCommonResult();
     }
 }
---
+++ Services/Chats/Apps.Chats/ChatMessages/Commands/MarkMessagesAsRead.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Chats.ChatMessages.Commands;
+public sealed record MarkMessagesAsRead(Guid ChatItemId) : IRequest<ResultStatus> {
+    public static MarkMessagesAsRead New(Guid chatItemId) => new(chatItemId);
+}
+
+//=========================== handler
+internal sealed class MarkMessagesAsReadHandler(IChatUOW _unitOfWork) : IRequestHandler<MarkMessagesAsRead , ResultStatus> {
+    public async Task<ResultStatus> Handle(MarkMessagesAsRead request , CancellationToken cancellationToken) {
+        var unreadMessages = (await _unitOfWork.Queries.ChatMessages.GetUnreadMessagesAsync(request.ChatItemId));
+        foreach (var unreadMessage in unreadMessages) {
+            unreadMessage.MarkAsRead();
+        }
+        await _unitOfWork.SaveChangeAsync();
+        return SuccessResults.Ok("All Messages marked as read.");
+    }
+}
