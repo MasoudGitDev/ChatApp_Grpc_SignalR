b6dc14e657f65414428ea18719e0d32456406a9d
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-11 16:16:42 +0330

    update : remove Created and Removed static members

--- Shared.Server/Models/Results/SuccessResults.cs
+++ Shared.Server/Models/Results/SuccessResults.cs
@@ -2,17 +2,12 @@
 
 public record class SuccessResults(string Code , string Message) {
     // for commands
-    public static ResultStatus Removed(Guid id)
-        => new(true , [MessageDescription.Create(nameof(Removed) , $"The record with id :<{id}> is removed successfully.")]);
-
-    public static ResultStatus Created
-        => new(true , [MessageDescription.Create(nameof(Created) , "The new record is created successfully.")]);
-
     public static ResultStatus Ok(string message)
         => new(true , [MessageDescription.Create("Ok" , message)]);
 
     // for queries
-    public static ResultStatus<T> Ok<T>(T data) => new(true , [],data);
+    public static ResultStatus<T> Ok<T>(T data) 
+        => new(true , [],data);
     public static ResultStatus<T> Ok<T>(string message , T data)
     => new(true , [MessageDescription.Create( "Ok" , message)] , data);
 }
