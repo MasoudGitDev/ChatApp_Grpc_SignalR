08e1c330a79f98d9b19e9062687d4467f1d36b97
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-12 22:50:39 +0330

    update : add paginaion params

--- Services/Chats/Apps.Chats/ChatMessages/Queries/GetMessages.cs
+++ Services/Chats/Apps.Chats/ChatMessages/Queries/GetMessages.cs
@@ -5,8 +5,9 @@ using Shared.Server.Models.Results;
 using UnitOfWorks.Abstractions;
 
 namespace Apps.Chats.ChatMessages.Queries;
-public sealed record GetMessages(Guid ChatItemId) : IRequest<ResultStatus<List<GetMessageDto>>> {
-    public static GetMessages New(Guid chatItemId) => new(chatItemId);
+public sealed record GetMessages(Guid ChatItemId , int PageNumber = 1 , int PageSize = 50) : IRequest<ResultStatus<List<GetMessageDto>>> {
+    public static GetMessages New(Guid chatItemId , int pageNumber = 1 , int pageSize = 50)
+        => new(chatItemId , pageNumber , pageSize);
 }
 
 
@@ -14,7 +15,9 @@ public sealed record GetMessages(Guid ChatItemId) : IRequest<ResultStatus<List<G
 internal sealed class GetMessagesHandler(IChatUOW _unitOfWork) : IRequestHandler<GetMessages , ResultStatus<List<GetMessageDto>>> {
     public async Task<ResultStatus<List<GetMessageDto>>> Handle(GetMessages request , CancellationToken cancellationToken) {
         try {
-            var messages = await _unitOfWork.Queries.ChatMessages.GetAllAsync(request.ChatItemId);
+            var messages = await _unitOfWork.Queries.ChatMessages.GetAllAsync(request.ChatItemId,true,
+                request.PageNumber,
+                request.PageSize);
             return SuccessResults.Ok(messages.Adapt<List<GetMessageDto>>());
         }
         catch(Exception e) {
