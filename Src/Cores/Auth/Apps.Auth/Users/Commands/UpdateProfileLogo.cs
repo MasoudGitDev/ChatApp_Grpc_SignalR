0f298054a329b3e2a192a2a1683076fe7a0538bf
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-20 10:06:42 +0330

    create : UpdateProfileLogo to update logo

---
+++ Src/Cores/Auth/Apps.Auth/Users/Commands/UpdateProfileLogo.cs
@@ -0,0 +1,22 @@
+﻿using Domains.Auth.User.ValueObjects;
+using MediatR;
+using Shared.Server.Models.Results;
+using UnitOfWorks.Abstractions;
+
+namespace Apps.Auth.Users.Commands;
+public sealed record UpdateProfileLogo(Guid UserId , ImageUrl ImageUrl) : IRequest<ResultStatus> {
+    public static UpdateProfileLogo New(Guid userId , ImageUrl imageUrl) => new(userId , imageUrl);
+}
+
+//----------- handler
+internal sealed class UploadProfileLogoHandler(IChatUOW _unitOfWork) : IRequestHandler<UpdateProfileLogo , ResultStatus> {
+    public async Task<ResultStatus> Handle(UpdateProfileLogo request , CancellationToken cancellationToken) {
+        var findUser = await _unitOfWork.Queries.Users.FindByIdAsync(request.UserId);
+        if(findUser is null) {
+            return ErrorResults.NotFound($"The userId : <{request.UserId}> not found.");
+        }
+        findUser.Update(request.ImageUrl);
+        await _unitOfWork.SaveChangeAsync();
+        return SuccessResults.Ok("The new image has been updated successfully.");
+    }
+}
