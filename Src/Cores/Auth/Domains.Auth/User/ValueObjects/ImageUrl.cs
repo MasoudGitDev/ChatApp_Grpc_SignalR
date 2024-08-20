b9004f49ef9ddd2bdd2ebbd847bd977f162d418c
MasoudSh <Msh.Git.Dev@gmail.com>
2024-08-20 10:07:10 +0330

    create : ImageUrl as a valueObject

---
+++ Src/Cores/Auth/Domains.Auth/User/ValueObjects/ImageUrl.cs
@@ -0,0 +1,7 @@
+﻿namespace Domains.Auth.User.ValueObjects;  
+public class ImageUrl {
+    public string Url { get; private set; }
+    public ImageUrl(string url) => Url = url;
+    public static implicit operator ImageUrl(string url) => new (url);
+    public static implicit operator string(ImageUrl imageUrl) => imageUrl.Url;
+}
